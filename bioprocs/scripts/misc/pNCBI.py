d4b97cb3a2b54d8b9edf97ac3d00e93a6d2d809f
pwwang <pwwang@pwwang.com>
2018-06-23 22:23:44 -0700

    Add misc.pNCBI

---
+++ bioprocs/scripts/misc/pNCBI.py
@@ -0,0 +1,57 @@
+from eutils import Client
+from bioprocs.utils.tsvio import TsvWriter, TsvRecord
+
+term    = {{in.term | repr}}
+outfile = {{out.outfile | quote}}
+prog    = {{args.prog | quote}}
+apikey  = {{args.apikey | quote}} or None
+db      = {{args.db | quote}}
+joiner  = {{args.joiner | quote}}
+record  = {{args.record}}
+writer  = TsvWriter(outfile)
+
+def writerResults(rets):
+	keys = []
+	if rets:
+		ret  = rets[0]
+		keys = [key for key in dir(ret) if not key.startswith('_')]
+		writer.meta.add(*keys)
+		writer.writeHead()
+	for ret in rets:
+		r = TsvRecord()
+		for key in keys:
+			try:
+				r[key] = getattr(ret, key)
+			except:
+				r[key] = ''
+			if isinstance(r[key], (list, tuple)):
+				try:
+					r[key] = joiner.join([str(_) for _ in r[key]])
+				except UnicodeEncodeError:
+					r[key] = joiner.join([_.encode('utf-8') for _ in r[key]])
+			else:
+				try:
+					r[key] = str(r[key])
+				except UnicodeEncodeError:
+					r[key] = r[key].encode('utf-8')
+					
+		if callable(record):
+			r = record(r)
+		elif record is not None:
+			raise ValueError('Unknown record transform function (args.record).')
+		if r:
+			writer.write(r)
+
+client = Client(api_key = apikey)
+if prog == 'esearch':
+	sret = client.esearch(db = db, term = term)
+	rets = client.efetch(db = db, id = sret.ids)
+	rets = list(iter(rets))
+	writerResults(rets)
+
+else:
+	fetches = client.efetch(db = db, id = term)
+	key     = [key for key in dir(fetches) if not key.startswith('_')][0]
+	rets    = getattr(fetches, key)
+	writerResults(rets)
+	
