9ce540438eb52d8efdb63a1d920b87e8fab191e4
pwwang <pwwang@pwwang.com>
2018-11-14 17:23:32 -0600

    Add cluster.pFeats2Dist

--- bioprocs/scripts/cluster/pFeats2Dist.r
+++ bioprocs/scripts/cluster/pFeats2Dist.r
@@ -7,6 +7,6 @@ sim     = {{args.sim | R}}
 
 indata = read.table(infile, header = inopts$cnames, row.names = if(inopts$rnames) 1 else NULL, sep = "\t", check.names = F)
 
-d = as.matrix(dist(indata, method = method))
+d = as.matrix(distance(indata, method = method))
 if (sim) d = 1 - d
 write.table(d, outfile, col.names = T, row.names = T, sep = "\t", quote = F)
