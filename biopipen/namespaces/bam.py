54f7155f47c560a2a4a1d5b6e49efdcc430e7b61
pwwang <pwwang@pwwang.com>
2021-11-08 13:30:02 -0600

    📝 Add docs for bam.CNVpytor

--- biopipen/namespaces/bam.py
+++ biopipen/namespaces/bam.py
@@ -5,7 +5,27 @@ from ..core.config import config
 
 
 class CNVpytor(Proc):
-    """Detect CNV using CNVpytor"""
+    """Detect CNV using CNVpytor
+
+    Input:
+        bamfile: The bam file
+        snpfile: The snp file
+
+    Output:
+        outdir: The output directory
+
+    Envs:
+        cnvpytor: Path to cnvpytor
+        ncores: Number of cores to use (`-j` for cnvpytor)
+        cases: Cases to run cnvpytor. A dictionary with keys as case names
+            and values is also a dict
+            - `chrom`: The chromosomes to run on
+            - `binsizes`: The binsizes
+            - `snp`: How to read snp data
+            - `mask_snps`: Whether mask 1000 Genome snps
+            - `baf_nomask`: Do not use P mask in BAF histograms
+
+    """
 
     input = "bamfile:file, snpfile:file"
     output = "outdir:dir:{{in.bamfile | stem}}.cnvpytor"
@@ -29,7 +49,7 @@ class CNVpytor(Proc):
                 "baf_nomask": False,
                 # other arguments for -rd
             }
-        }
+        },
     }
     script = "file://../scripts/bam/CNVpytor.py"
     plugin_opts = {"report": "file://../reports/bam/CNVpytor.svelte"}
