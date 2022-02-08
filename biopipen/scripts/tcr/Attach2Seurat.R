5a5369d29f4f5b202b8be538b453dc5b1747097d
pwwang <1188067+pwwang@users.noreply.github.com>
2022-02-08 16:09:05 -0700

    0.1.8 (#21)
    * ✨ Add tcr.Attach2Seurat
    
    * 🔖 0.1.8

---
+++ biopipen/scripts/tcr/Attach2Seurat.R
@@ -0,0 +1,43 @@
+library(Seurat)
+library(immunarch)
+library(glue)
+library(dplyr)
+library(tidyr)
+library(tibble)
+
+immfile = {{in.immfile | r}}
+sobjfile = {{in.sobjfile | r}}
+outfile = {{out.outfile | r}}
+metacols = {{envs.metacols | r}}
+
+immdata = readRDS(immfile)
+sobj = readRDS(sobjfile)
+
+metadf = do.call(rbind, lapply(seq_len(nrow(immdata$meta)), function(i) {
+    # Clones  Proportion   CDR3.aa                       Barcode
+    # 5      4 0.008583691 CAVRDTGNTPLVF;CASSEYSNQPQHF   GTTCGGGCACTTACGA-1;TCTCTAAGTACCAGTT-1
+    # 6      4 0.008583691 CALTQAAGNKLTF;CASRPEDLRGQPQHF GCTTGAAGTCGGCACT-1;TACTCGCTCCTAAGTG-1
+    cldata = immdata$data[[i]][, c(metacols, "Barcode")]
+    # # A tibble: 4 × 5
+    # Sample                  Patient     Timepoint Tissue
+    # <chr>                   <chr>       <chr>     <chr>
+    # 1 MC1685Pt011-Baseline-PB MC1685Pt011 Baseline  PB
+    mdata = as.list(immdata$meta[i, ])
+    for (mname in names(mdata)) {
+        assign(mname, mdata[[mname]])
+    }
+
+    cldata %>%
+        separate_rows(Barcode, sep=";") %>%
+        mutate(Barcode = glue("{{envs.prefix}}{Barcode}"))
+
+}))
+
+sobj@meta.data = left_join(
+    sobj@meta.data %>% rownames_to_column("Barcode"),
+    metadf,
+    by = "Barcode"
+) %>% column_to_rownames("Barcode")
+
+
+saveRDS(sobj, outfile)
