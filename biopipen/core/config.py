2e7045f1c083a08945994ce7334e84d687a6e07e
pwwang <pwwang@pwwang.com>
2022-05-15 02:12:17 -0500

    ⬆️ Upgrade pipen to v0.3

--- biopipen/core/config.py
+++ biopipen/core/config.py
@@ -5,33 +5,13 @@ from typing import Any
 from pathlib import Path
 from tempfile import gettempdir
 
-from simpleconf import Config as SimConfig
+from simpleconf import Config
 
 from .defaults import BIOPIPEN_DIR
 
 DEFAULT_CONFIG_FILE = BIOPIPEN_DIR / "core" / "config.toml"
 USER_CONFIG_FILE = Path("~").expanduser() / ".biopipen.toml"
 PROJ_CONFIG_FILE = Path(".") / ".biopipen.toml"
-
-
-class Config(SimConfig):
-    """Subclass the Config class to enables default value instead of
-    KeyError for non-exist keys"""
-
-    def __getattr__(self, name: str) -> Any:
-        try:
-            return super().__getattr__(name)
-        except (KeyError, AttributeError):
-            return None
-
-    def __getitem__(self, name: str) -> Any:
-        try:
-            return super().__getitem__(name)
-        except (KeyError, AttributeError):
-            return None
-
-
-config = Config(with_profile=False)
 
 config_profiles = [
     {"path": {"tmpdir": gettempdir()}},
@@ -44,4 +24,4 @@ if "+config" in sys.argv:
     cindex = sys.argv.index("+config")
     config_profiles.append(sys.argv[cindex + 1])
 
-config._load(*config_profiles)
+config = Config.load(*config_profiles, ignore_nonexist=True)
