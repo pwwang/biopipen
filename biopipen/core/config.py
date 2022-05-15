5485be18f04db92dbae14b9c9bdfc4c72b703db8
pwwang <pwwang@pwwang.com>
2022-05-15 02:27:37 -0500

    🐛 Default value to None when config key doesn't exist

--- biopipen/core/config.py
+++ biopipen/core/config.py
@@ -5,6 +5,7 @@ from typing import Any
 from pathlib import Path
 from tempfile import gettempdir
 
+from diot import Diot
 from simpleconf import Config
 
 from .defaults import BIOPIPEN_DIR
@@ -12,6 +13,25 @@ from .defaults import BIOPIPEN_DIR
 DEFAULT_CONFIG_FILE = BIOPIPEN_DIR / "core" / "config.toml"
 USER_CONFIG_FILE = Path("~").expanduser() / ".biopipen.toml"
 PROJ_CONFIG_FILE = Path(".") / ".biopipen.toml"
+
+
+class ConfigItems(Diot):
+    """Provides the envs from configuration files and defaults the
+    non-existing values to None."""
+
+
+    def __getattr__(self, name: str) -> Any:
+        try:
+            return super().__getattr__(name)
+        except (KeyError, AttributeError):
+            return None
+
+    def __getitem__(self, name: str) -> Any:
+        try:
+            return super().__getitem__(name)
+        except (KeyError, AttributeError):
+            return None
+
 
 config_profiles = [
     {"path": {"tmpdir": gettempdir()}},
@@ -24,4 +44,4 @@ if "+config" in sys.argv:
     cindex = sys.argv.index("+config")
     config_profiles.append(sys.argv[cindex + 1])
 
-config = Config.load(*config_profiles, ignore_nonexist=True)
+config = ConfigItems(Config.load(*config_profiles, ignore_nonexist=True))
