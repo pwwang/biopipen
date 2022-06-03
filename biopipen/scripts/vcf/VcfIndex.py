f4511061bd2986a846ff1a6b216c49a88b250f4a
pwwang <pwwang@pwwang.com>
2022-06-03 00:31:31 -0500

    🐛 [vcf.VcfIndex] Fix vcf.VcfIndex

--- biopipen/scripts/vcf/VcfIndex.py
+++ biopipen/scripts/vcf/VcfIndex.py
@@ -1,13 +1,14 @@
+from pathlib import Path
 from os import path
 
 import cmdy
 from biopipen.utils.reference import tabix_index
 
-infile = {{in.infile | repr}}
-outfile = {{out.outfile | repr}}
-outidx = {{out.outidx | repr}}
-tabix = {{envs.tabix | repr}}
-ncores = {{envs.ncores | repr}}
+infile = {{in.infile | repr}}  # pyright: ignore
+outfile = Path({{out.outfile | repr}})  # pyright: ignore
+outidx = {{out.outidx | repr}}  # pyright: ignore
+tabix = {{envs.tabix | repr}}  # pyright: ignore
+ncores = {{envs.ncores | repr}}  # pyright: ignore
 
 outfile_with_index = tabix_index(infile, "vcf", outfile.parent, tabix)
 if path.samefile(infile, outfile_with_index):
