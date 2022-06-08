60177de5cd2d512679b21e9a07c00269ed687e8a
pwwang <pwwang@pwwang.com>
2022-06-08 18:24:52 -0500

    💚 Install rtoml from wheel

--- tests/entrypoint.sh
+++ tests/entrypoint.sh
@@ -2,9 +2,12 @@
 
 cd /workdir
 
+pip install -U pip
 pip install -U poetry
 poetry config virtualenvs.create false
 
+# Use wheel to skip building from rust, which takes too long
+pip install -U rtoml
 poetry install
 
 python $1
