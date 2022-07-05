3f79dd099755140fd26f6852e2f99815bcecb69b
pwwang <pwwang@pwwang.com>
2022-07-05 15:25:52 -0500

    ⚡️ [scrna.ScFGSEA] Make ident hierarchical

--- biopipen/reports/scrna/ScFGSEA.svelte
+++ biopipen/reports/scrna/ScFGSEA.svelte
@@ -5,10 +5,20 @@
 </script>
 
 {%- macro report_job(job, h=1) -%}
-{% for casedir in job.out.outdir | joinpaths: "*" | glob %}
-{%  set case = casedir | basename %}
-<h{{h}}>{{case}}</h{{h}}>
-{{ fgsea_report(casedir, h + 1) }}
+{% for casedir in job.out.outdir | glob: "*" %}
+    {%- set case = casedir | basename -%}
+    <h{{h}}>{{case}}</h{{h}}>
+    {%- if casedir | joinpaths: "percluster" | as_path | attr: "is_file" | call -%}
+        {%- for cldir in casedir | as_path | attr: "glob" | call: "*" -%}
+            {%- if basename(cldir) == "percluster" -%}
+                {%- continue -%}
+            {%- endif -%}
+            <h{{h+1}}>{{cldir | basename}}</h{{h+1}}>
+            {{ fgsea_report(cldir, h + 2) }}
+        {%- endfor -%}
+    {%- else -%}
+        {{ fgsea_report(casedir, h + 1) }}
+    {%- endif -%}
 {% endfor %}
 {%- endmacro -%}
 
--- biopipen/scripts/scrna/ScFGSEA.R
+++ biopipen/scripts/scrna/ScFGSEA.R
@@ -46,8 +46,10 @@ prepare_exprmat = function(casepms) {
 }
 
 do_case = function(case, casepms) {
+    print(paste("- Processing case:", case, "..."))
     odir = file.path(outdir, case)
     dir.create(odir, showWarnings = FALSE)
+
     exprinfo = prepare_exprmat(casepms)
     ranks = prerank(
         exprinfo$exprs,
@@ -80,7 +82,55 @@ do_case = function(case, casepms) {
 
 }
 
+do_case_with_ident = function(case, casepms, ident) {
+    print(paste("- Processing case:", case, "..."))
+    print(paste("  Cluster:", ident, "..."))
+    ident_name = as.integer(ident)
+    if (is.integer(ident_name)) {
+        ident_name = paste0("Cluster", ident)
+    }
+    odir = file.path(outdir, case, ident_name)
+    dir.create(odir, showWarnings = FALSE, recursive = TRUE)
+    cat("", file = file.path(outdir, case, "percluster"))
+
+    exprinfo = prepare_exprmat(casepms)
+    ranks = prerank(
+        exprinfo$exprs,
+        casepms$ident.1,
+        casepms$ident.2,
+        exprinfo$allclasses,
+        envs$method
+    )
+
+    write.table(
+        ranks,
+        file.path(odir, "fgsea.rank"),
+        row.names=F,
+        col.names=T,
+        sep="\t",
+        quote=F
+    )
+
+    case_envs = envs
+    top = case_envs$top
+    case_envs$name = NULL
+    case_envs$cases = NULL
+    case_envs$gmtfile = NULL
+    case_envs$nproc = case_envs$ncores
+    case_envs$method = NULL
+    case_envs$ncores = NULL
+    case_envs$top = NULL
+    # the rest are the arguments for `fgsea()`
+    runFGSEA(ranks, gmtfile, top, odir, case_envs)
+
+}
+
+
+
 .replace_placeholder = function(s, ident) {
+    if (is.null(s)) {
+        return(s)
+    }
     s = sub("{ident}", ident, s, fixed = TRUE)
     s = sub("{cluster}", ident, s, fixed = TRUE)
     s
@@ -88,23 +138,25 @@ do_case = function(case, casepms) {
 
 do_case_with_tpl = function(case_with_tpl) {
     casepms = cases$cases[[case_with_tpl]]
-    if (grepl("{", case_with_tpl, fixed = TRUE)) {
+    if (isTRUE(casepms$percluster)) {
         # has template in case names
         # currently only cluster is supported
-        for (ident in unique(Idents(srtobj))) {
-            case = .replace_placeholder(case_with_tpl, ident)
-            casepms$ident.1 = .replace_placeholder(casepms$ident.1, ident)
-            casepms$ident.2 = .replace_placeholder(casepms$ident.2, ident)
-            casepms$group.by = .replace_placeholder(casepms$group.by, ident)
+        casepms1 = casepms
+        casepms1$percluster = NULL
+        for (ident in sort(unique(Idents(srtobj)))) {
+            casepms1$ident.1 = .replace_placeholder(casepms$ident.1, ident)
+            casepms1$ident.2 = .replace_placeholder(casepms$ident.2, ident)
+            casepms1$group.by = .replace_placeholder(casepms$group.by, ident)
+            casepms1$filter = .replace_placeholder(casepms$filter, ident)
             if (!is.null(casepms$mutaters)) {
                 for (mutname in names(casepms$mutaters)) {
-                    casepms$mutaters[[mutname]] = .replace_placeholder(
+                    casepms1$mutaters[[mutname]] = .replace_placeholder(
                         casepms$mutaters[[mutname]],
                         ident
                     )
                 }
             }
-            do_case(case, casepms)
+            do_case_with_ident(case_with_tpl, casepms1, ident)
         }
     } else {
         do_case(case_with_tpl, casepms)
