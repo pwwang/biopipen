1fca919a20d8d1b966119e6044370eaf9a24eaaf
pwwang <pwwang@pwwang.com>
2023-01-06 13:50:30 -0600

    🚨 Ignore template strings in python scripts for pyright

--- biopipen/scripts/bcftools/BcftoolsAnnotate.py
+++ biopipen/scripts/bcftools/BcftoolsAnnotate.py
@@ -3,16 +3,16 @@ from os import path
 import cmdy
 from biopipen.utils.reference import tabix_index
 
-infile = {{in.infile | repr}}
-annfile = {{(in.annfile or envs.annfile) | repr}}
-outfile = {{out.outfile | repr}}
-joboutdir = {{job.outdir | repr}}
-bcftools = {{envs.bcftools | repr}}
-tabix = {{envs.tabix | repr}}
-ncores = {{envs.ncores | repr}}
-cols = {{envs.cols | repr}}
-header = {{envs.header | repr}}
-args = {{envs.args | repr}}
+infile = {{in.infile | repr}}  # pyright: ignore
+annfile = {{(in.annfile or envs.annfile) | repr}}  # pyright: ignore
+outfile = {{out.outfile | repr}}  # pyright: ignore
+joboutdir = {{job.outdir | repr}}  # pyright: ignore
+bcftools = {{envs.bcftools | repr}}  # pyright: ignore
+tabix = {{envs.tabix | repr}}  # pyright: ignore
+ncores = {{envs.ncores | repr}}  # pyright: ignore
+cols = {{envs.cols | repr}}  # pyright: ignore
+header = {{envs.header | repr}}  # pyright: ignore
+args = {{envs.args | repr}}  # pyright: ignore
 
 args["_exe"] = bcftools
 args["_"] = tabix_index(infile, "vcf")
--- biopipen/scripts/misc/Str2File.py
+++ biopipen/scripts/misc/Str2File.py
@@ -1,6 +1,6 @@
-instr = {{in.str | repr}}
-name = {{repr(in.name or envs.name)}}
-outfile = {{out.outfile | quote}}
+instr = {{in.str | repr}}  # pyright: ignore
+name = {{repr(in.name or envs.name)}}  # pyright: ignore
+outfile = {{out.outfile | quote}}  # pyright: ignore
 
 with open(outfile, "wt") as fout:
     fout.write(instr)
