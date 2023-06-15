4db778438e82fb05f0a7ec4bc3d7b2a43bc9cab0
pwwang <pwwang@pwwang.com>
2023-06-15 16:27:06 -0500

    ⚙️[cnvkit_pipeline] Make examples

--- docker/cnvkit_pipeline/make-examples.sh
+++ docker/cnvkit_pipeline/make-examples.sh
@@ -17,15 +17,12 @@ echo "| Donwloading test data                  |"
 echo "+----------------------------------------+"
 echo ""
 for tfile in \
-    reference/human_g1k_v37_decoy.small.fasta \
-    testdata/target.bed \
-    testdata/recalbam/9876T.recal.bam \
-    testdata/recalbam/9876T.recal.bai \
-    testdata/recalbam/1234N.recal.bam \
-    testdata/recalbam/1234N.recal.bai;
+    genome/genome.fasta \
+    illumina/bam/test.paired_end.sorted.bam \
+    illumina/bam/test2.paired_end.sorted.bam;
 do
     if [[ ! -f $WORKDIR/$tfile ]]; then
-        wget -q https://raw.githubusercontent.com/nf-core/test-datasets/sarek/$tfile -O $WORKDIR/$(basename $tfile)
+        wget -q https://raw.githubusercontent.com/nf-core/test-datasets/modules/data/genomics/homo_sapiens/$tfile -O $WORKDIR/$(basename $tfile)
     fi
 done
 
@@ -34,7 +31,7 @@ echo "| Donwloading refFlat                    |"
 echo "+----------------------------------------+"
 echo ""
 if [[ ! -f $WORKDIR/refFlat.txt ]]; then
-    wget -q https://hgdownload.cse.ucsc.edu/goldenpath/hg19/database/refFlat.txt.gz -O $WORKDIR/refFloat.txt.gz
+    wget -q https://hgdownload.cse.ucsc.edu/goldenpath/hg19/database/refFlat.txt.gz -O $WORKDIR/refFlat.txt.gz
     gunzip $WORKDIR/refFlat.txt.gz
 fi
 
@@ -44,9 +41,9 @@ echo "+----------------------------------------+"
 echo ""
 
 metafile=$WORKDIR/metadata.txt
-echo -e "Sample\tBam\tGroup" > $metafile
-echo -e "9876T\t$WORKDIR/9876T.recal.bam\tTumor" >> $metafile
-echo -e "1234N\t$WORKDIR/1234N.recal.bam\tNormal" >> $metafile
+echo -e "Sample\tBam\tGuessBaits" > $metafile
+echo -e "Test\t$WORKDIR/test.paired_end.sorted.bam\t1" >> $metafile
+echo -e "Test2\t$WORKDIR/test2.paired_end.sorted.bam\t1" >> $metafile
 
 echo "+----------------------------------------+"
 echo "| Preparing the data for pipen-board     |"
