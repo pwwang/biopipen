7f68776c369901c91d3390d9b1b69374ff18a8e9
pwwang <pwwang@pwwang.com>
2023-09-06 17:58:09 -0500

    ✨ [scrna.MarkersFinder] Allow to use mutate_helpers in envs.mutaters

--- biopipen/ns/scrna.py
+++ biopipen/ns/scrna.py
@@ -534,6 +534,24 @@ class MarkersFinder(Proc):
             `future::plan(strategy = "multicore", workers = ncores)`.
             See also: https://satijalab.org/seurat/articles/future_vignette.html
         mutaters (type=json): The mutaters to mutate the metadata
+            There are also also 4 helper functions, `expanded`, `collapsed`, `emerged` and `vanished`, that can be used to identify the expanded/collpased/emerged/vanished groups (i.e. TCR clones).
+            For example, you can use `{"Patient1_Tumor_Collapsed_Clones": "expanded(Source, 'Tumor', subset = Patent == 'Patient1')"}`
+            to create a new column in metadata named `Patient1_Tumor_Collapsed_Clones`
+            with the collapsed clones in the tumor sample (compared to the normal sample) of patient 1. The values in this columns for other clones will be `NA`.
+            Those functions take following arguments:
+            * `df`: The metadata data frame. You can use the `.` to refer to it.
+            * `group-by`: The column name in metadata to group the cells.
+            * `idents`: The first group or both groups of cells to compare (value in `group-by` column). If only the first group is given, the rest of the cells (with non-NA in `group-by` column) will be used as the second group.
+            * `subset`: An expression to subset the cells, will be passed to `dplyr::filter()`. Default is `TRUE` (no filtering).
+            * `id`: The column name in metadata for the group ids (i.e. `CDR3.aa`)
+            * `compare`: Either a (numeric) column name (i.e. `Clones`) in metadata to compare between groups, or `.n` to compare the number of cells in each group.
+            * `uniq`: Whether to return unique ids or not. Default is `TRUE`. If `FALSE`, you can mutate the meta data frame with the returned ids. For example, `df %>% mutate(expanded = expanded(...))`.
+            * `order`: The order of the returned ids. It could be `sum` or `diff`, which is the sum or diff of the `compare` between idents.
+                Two kinds of modifiers can be added, including `desc` and `abs`.
+                For example, `sum,desc` means the sum of `compare` between idents in descending order.
+                Default is `diff,abs,desc`. It only works when `uniq` is `TRUE`. If `uniq` is `FALSE`, the returned
+                ids will be in the same order as in `df`.
+            Note that the numeric column should be the same for all cells in the same group. This will not be checked (only the first value is used).
         ident-1: The first group of cells to compare
         ident-2: The second group of cells to compare
             If not provided, the rest of the cells are used for `ident-2`.
@@ -572,8 +590,8 @@ class MarkersFinder(Proc):
             If no cases are specified, the default case will be added with
             the default values under `envs` with the name `Cluster`.
     """  # noqa: E501
-    input = "srtobj:file, casefile:file"
-    output = "outdir:dir:{{(in.casefile or in.srtobj) | stem0}}.markers"
+    input = "srtobj:file"
+    output = "outdir:dir:{{in.srtobj | stem0}}.markers"
     lang = config.lang.rscript
     envs = {
         "ncores": config.misc.ncores,
