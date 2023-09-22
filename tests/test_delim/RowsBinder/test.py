26b568c354455d464e3e0e259d1cc7891211470c
pwwang <pwwang@pwwang.com>
2023-09-22 01:48:28 -0500

    ✨ [delim.SampleInfo] Add process

--- tests/test_delim/RowsBinder/test.py
+++ tests/test_delim/RowsBinder/test.py
@@ -1,6 +1,9 @@
 from pathlib import Path
 
-from biopipen.ns.delim import RowsBinder as RowsBinder_
+from biopipen.ns.delim import (
+    RowsBinder as RowsBinder_,
+    SampleInfo as SampleInfo_,
+)
 from biopipen.core.testing import get_pipeline
 
 
@@ -18,6 +21,88 @@ class RowsBinderWithFilenameStrings(RowsBinder_):
 
 class RowsBinderWithFilenameFuns(RowsBinder_):
     envs = {"filenames": "function(x) tools::file_path_sans_ext(basename(x))"}
+
+
+class SampleInfo(SampleInfo_):
+    requires = RowsBinderWithFilenameFuns
+    envs = {
+        "mutaters": {"Part": "paste0('Part_', Filename)"},
+        "stats": {
+            "Samples_Source": {
+                "group": "Source",
+            },
+            "Samples_Subject": {
+                "group": "Subject",
+                "plot": "bar",
+            },
+            "Samples_Source_each_Subject": {
+                "group": "Source",
+                "each": "Subject",
+            },
+            "Samples_Subject_each_Source": {
+                "group": "Subject",
+                "plot": "bar",
+                "each": "Source",
+            },
+            "Score": {
+                "on": "Score",
+            },
+            "Score_Source": {
+                "on": "Score",
+                "group": "Source",
+            },
+            "Score_Source_each_Subject": {
+                "on": "Score",
+                "group": "Source",
+                "each": "Subject",
+            },
+            "Score_violin": {
+                "on": "Score",
+                "plot": "violin",
+            },
+            "Score_Source_violin": {
+                "on": "Score",
+                "group": "Source",
+                "plot": "violin",
+            },
+            "Score_Source_each_Subject_violin": {
+                "on": "Score",
+                "group": "Source",
+                "each": "Subject",
+                "plot": "violin",
+            },
+            "Score_vlnbox": {
+                "on": "Score",
+                "plot": "violin+box",
+            },
+            "Score_Source_vlnbox": {
+                "on": "Score",
+                "group": "Source",
+                "plot": "violin+box",
+            },
+            "Score_Source_each_Subject_vlnbox": {
+                "on": "Score",
+                "group": "Source",
+                "each": "Subject",
+                "plot": "violin+box",
+            },
+            "Score_hist": {
+                "on": "Score",
+                "plot": "hist",
+            },
+            "Score_Source_hist": {
+                "on": "Score",
+                "group": "Source",
+                "plot": "hist",
+            },
+            "Score_Source_each_Subject_hist": {
+                "on": "Score",
+                "group": "Source",
+                "each": "Subject",
+                "plot": "hist",
+            },
+        }
+    }
 
 
 def pipeline():
