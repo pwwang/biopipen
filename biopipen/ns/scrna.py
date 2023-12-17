cef71eedff752f68348ada5d337f52f271130ca1
pwwang <pwwang@pwwang.com>
2023-12-17 01:38:17 -0600

    scrna.MarkersFinder: Add `envs.use_presto` to use presto to speed up finding markers

--- biopipen/ns/scrna.py
+++ biopipen/ns/scrna.py
@@ -877,9 +877,14 @@ class MarkersFinder(Proc):
             If `each` is specified, the section name will be constructed from
             `each` and case name.
         subset: An expression to subset the cells for each case.
+        use_presto: Whether to use [`presto::wilcoxauc`](https://rdrr.io/github/immunogenomics/presto/man/wilcoxauc.html)
+            to find markers.
+            [`presto`](https://github.com/immunogenomics/presto) is a package performs
+            fast Wilcoxon rank sum test and auROC analysis.
         rest (ns): Rest arguments for `Seurat::FindMarkers()`.
             Use `-` to replace `.` in the argument name. For example,
             use `min-pct` instead of `min.pct`.
+            This only works when `use_presto` is `False`.
             - <more>: See <https://satijalab.org/seurat/reference/findmarkers>
         dotplot (ns): Arguments for `Seurat::DotPlot()`.
             Use `-` to replace `.` in the argument name. For example,
@@ -913,6 +918,7 @@ class MarkersFinder(Proc):
         "section": "DEFAULT",
         "assay": None,
         "subset": None,
+        "use_presto": False,
         "rest": {},
         "dbs": [
             "GO_Biological_Process_2021",
