96e15e0cfe4609ea02bc873cc604491831970832
pwwang <pwwang@pwwang.com>
2023-12-25 16:12:19 -0600

    scrna.SeuratSubClustering: Add process

--- biopipen/ns/scrna.py
+++ biopipen/ns/scrna.py
@@ -301,6 +301,80 @@ class SeuratClustering(Proc):
     script = "file://../scripts/scrna/SeuratClustering.R"
 
 
+class SeuratSubClustering(Proc):
+    """Find clusters of a subset of cells.
+
+    It's unlike [`Seurat::FindSubCluster`], which only finds subclusters of a single
+    cluster. Instead, it will perform the whole clustering procedure on the subset of
+    cells. One can use metadata to specify the subset of cells to perform clustering on.
+
+    For the subset of cells, the reductions will be re-performed on the subset of cells,
+    and then the clustering will be performed on the subset of cells. The reduction
+    will be saved in `sobj@reduction$sub_umap_<casename>` of the original object and the
+    clustering will be saved in the metadata of the original object using the casename \
+    as the column name.
+
+    Input:
+        srtobj: The seurat object
+
+    Output:
+        rdsfile: The seurat object with the subclustering information.
+
+    Envs:
+        mutaters (type=json): The mutaters to mutate the metadata to subset the cells.
+            The mutaters will be applied in the order specified.
+        subset: An expression to subset the cells, will be passed to
+            [`tidyseurat::filter()`](https://stemangiola.github.io/tidyseurat/reference/filter.html).
+
+        RunUMAP (ns): Arguments for [`RunUMAP()`](https://satijalab.org/seurat/reference/runumap).
+            `object` is specified internally as the subset object, and `-` in the key will be replaced with `.`.
+            `dims=N` will be expanded to `dims=1:N`; The maximal value of `N` will be the minimum of `N` and the number of columns - 1 for each sample.
+            - dims (type=int): The number of PCs to use
+            - reduction: The reduction to use for UMAP.
+                If not provided, `sobj@misc$integrated_new_reduction` will be used.
+            - <more>: See <https://satijalab.org/seurat/reference/runumap>
+        FindNeighbors (ns): Arguments for [`FindNeighbors()`](https://satijalab.org/seurat/reference/findneighbors).
+            `object` is specified internally, and `-` in the key will be replaced with `.`.
+            - reduction: The reduction to use.
+                If not provided, `sobj@misc$integrated_new_reduction` will be used.
+            - <more>: See <https://satijalab.org/seurat/reference/findneighbors>
+        FindClusters (ns): Arguments for [`FindClusters()`](https://satijalab.org/seurat/reference/findclusters).
+            `object` is specified internally, and `-` in the key will be replaced with `.`.
+            - resolution: The resolution of the clustering. You can have multiple resolutions separated by comma.
+                The results will be saved in `<casename>_<resolution>`.
+                The final resolution will be used to define the clusters at `<casename>`.
+            - <more>: See <https://satijalab.org/seurat/reference/findclusters>
+        cache (type=auto): Whether to cache the seurat object with cluster information.
+            If `True`, the seurat object will be cached in the job output directory, which will be not cleaned up when job is rerunning.
+            The cached seurat object will be saved as `<signature>.cached.RDS` file, where `<signature>` is the signature determined by
+            the input and envs of the process.
+            See -
+            * <https://github.com/satijalab/seurat/issues/7849>
+            * <https://github.com/satijalab/seurat/issues/5358> and
+            * <https://github.com/satijalab/seurat/issues/6748> for more details.
+            To not use the cached seurat object, you can either set `cache` to `False` or delete the cached file at
+            `<signature>.cached.RDS` in the cache directory.
+            If `True`, the cache directory is `.pipen/<Pipeline>/SeuratClustering/0/output/`
+            You can also specify customized directory to save the cached seurat object by setting `cache` to the directory path.
+        cases (type=json): The cases to perform subclustering.
+            Keys are the names of the cases and values are the dicts inherited from `envs` except `mutaters` and `cache`.
+            If empty, a case with name `subcluster` will be created with default parameters.
+    """  # noqa: E501
+    input = "srtobj:file"
+    output = "rdsfile:file:{{in.srtobj | stem}}.RDS"
+    lang = config.lang.rscript
+    envs = {
+        "mutaters": {},
+        "subset": None,
+        "RunUMAP": {"dims": 30},
+        "FindNeighbors": {},
+        "FindClusters": {"resolution": 0.8},
+        "cache": True,
+        "cases": {"subcluster": {}},
+    }
+    script = "file://../scripts/scrna/SeuratSubClustering.R"
+
+
 class SeuratClusterStats(Proc):
     """Statistics of the clustering.
 
---
+++ biopipen/scripts/scrna/SeuratSubClustering.R
@@ -0,0 +1,175 @@
+source("{{biopipen_dir}}/utils/misc.R")
+
+library(Seurat)
+library(future)
+library(rlang)
+library(tidyr)
+library(dplyr)
+library(tidyseurat)
+library(digest)
+
+set.seed(8525)
+
+srtfile <- {{in.srtobj | quote}}
+rdsfile <- {{out.rdsfile | quote}}
+joboutdir <- {{job.outdir | quote}}
+envs <- {{envs | r: todot = "-"}}
+
+options(str = strOptions(vec.len = 5, digits.d = 5))
+options(future.globals.maxSize = 80000 * 1024^2)
+plan(strategy = "multicore", workers = envs$ncores)
+
+.expand_dims <- function(args, name = "dims") {
+    # Expand dims from 30 to 1:30
+    if (is.numeric(args[[name]]) && length(args[[name]] == 1)) {
+        args[[name]] <- 1:args[[name]]
+    }
+    args
+}
+
+envs$RunUMAP <- .expand_dims(envs$RunUMAP)
+envs$FindNeighbors <- .expand_dims(envs$FindNeighbors)
+
+log_info("Reading Seurat object ...")
+srtobj <- readRDS(srtfile)
+
+if (isTRUE(envs$cache)) {
+    envs$cache <- joboutdir
+}
+
+if (is.character(envs$cache) && nchar(envs$cache) > 0) {
+    log_info("Obtainning the signature ...")
+    envs2 <- envs
+    envs2$ncores <- NULL
+    sig <- c(
+        capture.output(str(srtobj)),
+        "\n\n-------------------\n\n",
+        capture.output(str(envs2)),
+        "\n"
+    )
+    digested_sig <- digest::digest(sig, algo = "md5")
+    cached_file <- file.path(envs$cache, paste0(digested_sig, ".cached.RDS"))
+    if (file.exists(cached_file)) {
+        log_info("Using cached results {cached_file}")
+        # copy cached file to rdsfile
+        file.copy(cached_file, rdsfile, copy.date = TRUE)
+        quit()
+    } else {
+        log_info("Cached results not found, logging the current and cached signatures.")
+        log_info("- Current signature:")
+        print(sig)
+        sigfiles <- Sys.glob(file.path(envs$cache, "*.signature.txt"))
+        for (sigfile in sigfiles) {
+            log_info("- Found cached signature file: {sigfile}")
+            cached_sig <- readLines(sigfile)
+            log_info("- Cached signature:")
+            print(cached_sig)
+        }
+        writeLines(sig, file.path(envs$cache, paste0(digested_sig, ".signature.txt")))
+    }
+}
+
+if (!is.null(envs$mutaters) && length(envs$mutaters) > 0) {
+    log_info("Mutating Seurat object ...")
+    srtobj@meta.data <- srtobj@meta.data %>%
+        mutate(!!!lapply(mutaters, parse_expr))
+}
+
+if (length(envs$cases) == 0) {
+    envs$cases <- list(
+        subcluster = list(
+            subset = envs$subset,
+            RunUMAP = envs$RunUMAP,
+            FindNeighbors = envs$FindNeighbors,
+            FindClusters = envs$FindClusters
+        )
+    )
+}
+
+for (key in names(envs$cases)) {
+    log_info("Running case '{key}' ...")
+    case <- envs$cases[[key]]
+
+    if (is.null(case$subset) || length(case$subset) == 0) {
+        stop(paste0("`subset` for case '", key, "' is empty."))
+    }
+
+    log_info("- Subsetting ...")
+    sobj <- srtobj %>% filter(!!parse_expr(case$subset))
+
+    log_info("- Running RunUMAP ...")
+    umap_args <- list_setdefault(
+        case$RunUMAP,
+        object = sobj,
+        dims = 1:30,
+        reduction = sobj@misc$integrated_new_reduction
+    )
+    umap_args$dims <- 1:min(max(umap_args$dims), ncol(sobj) - 1)
+    sobj <- do_call(RunUMAP, umap_args)
+
+    log_info("- Running FindNeighbors ...")
+    case$FindNeighbors$object <- sobj
+    if (is.null(case$FindNeighbors$reduction)) {
+        case$FindNeighbors$reduction <- sobj@misc$integrated_new_reduction
+    }
+    sobj <- do_call(FindNeighbors, case$FindNeighbors)
+
+    log_info("- Running FindClusters ...")
+    if (is.null(case$FindClusters$random.seed)) {
+        case$FindClusters$random.seed <- 8525
+    }
+    resolution <- case$FindClusters$resolution
+    if (is.character(resolution)) {
+        if (grepl(",", resolution)) {
+            resolution <- as.numeric(trimws(unlist(strsplit(resolution, ","))))
+        } else {
+            resolution <- as.numeric(resolution)
+        }
+    }
+    if (is.null(resolution) || length(resolution) == 1) {
+        case$FindClusters$resolution <- resolution
+        case$FindClusters$object <- sobj
+        case$FindClusters$cluster.name <- key
+        sobj <- do_call(FindClusters, case$FindClusters)
+        levels(sobj[[key]]) <- paste0("g", levels(sobj[[key]]))
+        ident_table <- table(sobj[[key]])
+        log_info("- Found {length(ident_table)} clusters:")
+        print(ident_table)
+
+        log_info("- Updating meta.data with subclusters...")
+        srtobj <- AddMetaData(srtobj, metadata = sobj@meta.data[, key, drop = FALSE])
+        srtobj@reductions[[paste0("sub_umap_", key)]] <- sobj@reductions$umap
+    } else {
+        log_info("- Multiple resolutions detected ...")
+        metadata <- NULL
+        sobj <- NULL
+        for (res in resolution) {
+            findclusters_args <- case$FindClusters
+            findclusters_args$resolution <- res
+            findclusters_args$object <- sobj
+            findclusters_args$cluster.name <- paste0(key, "_", res)
+            sobj <- do_call(FindClusters, findclusters_args)
+            res_key <- findclusters_args$cluster.name
+            sobj[[res_key]] <- Idents(sobj)
+            levels(sobj[[res_key]]) <- paste0("g", levels(sobj[[res_key]]))
+            ident_table <- table(sobj[[res_key]])
+            log_info("- Found {length(ident_table)} at resolution: {res}:")
+            print(ident_table)
+
+            log_info("- Updating meta.data with subclusters...")
+            metadata <- sobj@meta.data[, res_key, drop = FALSE]
+            srtobj <- AddMetaData(srtobj, metadata = metadata)
+            srtobj@reductions[[paste0("sub_umap_", res_key)]] <- sobj@reductions$umap
+        }
+        srtobj <- AddMetaData(srtobj, metadata = metadata, col.name = key)
+        srtobj@reductions[[paste0("sub_umap_", key)]] <- sobj@reductions$umap
+    }
+}
+
+log_info("Saving results ...")
+saveRDS(sobj, file = rdsfile)
+
+if (is.character(envs$cache) && nchar(envs$cache) > 0) {
+    log_info("Caching results ...")
+    file.copy(rdsfile, cached_file, overwrite = TRUE)
+}
