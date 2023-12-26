23952433ce07bf841e55ab1a60c3981aa15aeb93
pwwang <pwwang@pwwang.com>
2023-12-25 22:16:54 -0600

    scrna_metabolic_landscape.MetabolicPathwayActivity: Fix when default assay is SCT

--- biopipen/scripts/scrna_metabolic_landscape/MetabolicPathwayActivity.R
+++ biopipen/scripts/scrna_metabolic_landscape/MetabolicPathwayActivity.R
@@ -123,27 +123,25 @@ do_one_subset <- function(s, subset_col, subset_prefix) {
         genes_comm <- intersect(genes_comm, genes_expressed)
         if (length(genes_comm) < 5) next
 
-        pathway_metabolic_obj <- subset(subset_obj, features = genes_comm)
-        assay <- DefaultAssay(pathway_metabolic_obj)
-        mean_exp_eachCellType <- AverageExpression(pathway_metabolic_obj, assays = assay)[[assay]]
+        # Errored if default assay is SCT
+        # Issue with Seurat?
+        # pathway_metabolic_obj <- subset(subset_obj, features = genes_comm)
+        assay <- DefaultAssay(subset_obj)
+        mean_exp_eachCellType <- AverageExpression(subset_obj, features = genes_comm, assays = assay)[[assay]]
 
         # remove genes which are zeros in any celltype to avoid extreme ratio value
         keep <- rownames(mean_exp_eachCellType)[rowAlls(mean_exp_eachCellType > 0.001, useNames = F)]
         if (length(keep) < 3) next
 
         # using the loweset value to replace zeros for avoiding extreme ratio value
-        pathway_metabolic_obj <- subset(pathway_metabolic_obj, features = keep)
-        assay_data = GetAssayData(pathway_metabolic_obj)
+        # pathway_metabolic_obj <- subset(subset_obj, features = keep)
+        assay_data = GetAssayData(subset_obj, assay = assay, layer = "data")[keep, , drop = F]
         assay_data <- t(apply(assay_data, 1, function(x) {
             x[x <= 0] <- min(x[x > 0])
             x
         }))
-        pathway_metabolic_obj <- SetAssayData(
-            pathway_metabolic_obj,
-            assay = assay,
-            layer = "data",
-            new.data = assay_data
-        )
+        pathway_metabolic_obj <- CreateSeuratObject(CreateAssayObject(data = assay_data), assay = assay)
+        Idents(pathway_metabolic_obj) <- Idents(subset_obj)
         pathway_number_weight <- 1 / gene_pathway_number[keep, ]
         #
         mean_exp_eachCellType <- t(AverageExpression(pathway_metabolic_obj, assays = assay)[[assay]])
