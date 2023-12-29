c973bd3d827c077dd9b73a40b049c6186d5d04e1
pwwang <pwwang@pwwang.com>
2023-12-28 18:07:59 -0600

    scrna.SeuratClusterStats: Allow avgheatmap to plot features other than gene expressions

--- biopipen/scripts/scrna/SeuratClusterStats-features.R
+++ biopipen/scripts/scrna/SeuratClusterStats-features.R
@@ -175,6 +175,11 @@ do_one_features = function(name) {
         }
     } else if (case$kind == "avgheatmap") {
         case$kind = "avgheatmap"
+        excluded_args = c(
+            excluded_args,
+            "group.by", "split.by", "downsample", "ncol", "reduction", "layer",
+            "assay", "object"
+        )
         default_devpars = function(features, ncol) {
             list(
                 width = n_uidents * 30 + 350,
@@ -276,30 +281,55 @@ do_one_features = function(name) {
     case$features = .get_features(case$features)
     if (kind == "avgheatmap") {
         figfile <- file.path(odir, paste0(slugify(name), ".avgheatmap.png"))
-        assay <- DefaultAssay(case$object)
-        layer <- "scale.data"
-        if (!layer %in% Layers(case$object, assay = assay)) {
-            layer <- "data"
+        assay <- assay %||% DefaultAssay(object)
+        layer <- layer %||% ifelse("scale.data" %in% Layers(object, assay = assay), "scale.data", "data")
+
+        case_features <- case$features
+        case$features <- NULL
+        meta_feats <- intersect(case_features, colnames(object@meta.data))
+        expr_feats <- setdiff(case_features, meta_feats)
+        exprs <- NULL
+        if (length(meta_feats) > 0) {
+            exprs <- object@meta.data %>% select(all_of(c(meta_feats, ident))) %>%
+                group_by(!!sym(ident)) %>%
+                summarise(across(everything(), ~ mean(.x, na.rm = TRUE))) %>%
+                column_to_rownames(ident) %>%
+                t()
         }
-        exprs <- AverageExpression(
-            case$object,
-            assays = assay,
-            layer = layer,
-            features = case$features,
-            group.by = ident)[[assay]]
+        if (length(expr_feats)) {
+            exprs_tmp <- AverageExpression(
+                object,
+                assays = assay,
+                layer = layer,
+                features = expr_feats,
+                group.by = ident)[[assay]]
+            exprs <- bind_rows(exprs, exprs_tmp)
+        }
+
+        ha <- NULL
+        extra_height <- 0
+        extra_width <- 0  # legend
+        if (!is.null(cluster_order_val)) {
+            ha <- list()
+            ha[[cluster_orderby]] <- cluster_order_val
+            if (is.numeric(cluster_order_val)) {
+                col_fun <- colorRamp2(
+                    c(min(cluster_order_val), max(cluster_order_val)),
+                    c("lightyellow", "red"))
+                ha$col <- list()
+                ha$col[[cluster_orderby]] <- col_fun
+            }
+            ha <- do_call(HeatmapAnnotation, ha)
+            extra_height <- 40
+            extra_width <- 120
+        }
 
         col_fun <- colorRamp2(
             c(min(exprs, na.rm = T), 0, max(exprs, na.rm = T)),
             c("lightblue", "white", "darkred"))
 
-        ha <- list()
-        ha[[ident]] <- colnames(exprs)
-        ha$col <- list()
-        ha$col[[ident]] <- setNames(pal_biopipen()(length(ha[[ident]])), ha[[ident]])
-        ha <- do_call(HeatmapAnnotation, ha)
-
-        p <- Heatmap(
-            as.matrix(exprs),
+        case <- list_update(list(
+            matrix = as.matrix(exprs),
             name = "Average expression",
             col = col_fun,
             na_col = "white",
@@ -309,8 +339,13 @@ do_one_features = function(name) {
             rect_gp = gpar(col = "gray", lwd = 1),
             row_names_max_width = max_text_width(rownames(exprs)),
             top_annotation = ha
-        )
-        devpars = list_update(default_devpars(case$features, NULL), devpars)
+        ), case)
+        p <- do_call(Heatmap, case)
+
+        def_devpars = default_devpars(case_features, NULL)
+        def_devpars$width = def_devpars$width + extra_width
+        def_devpars$height = def_devpars$height + extra_height
+        devpars = list_update(def_devpars, devpars)
         png(figfile, res = devpars$res, width = devpars$width, height = devpars$height)
         print(p)
         dev.off()
@@ -324,8 +359,8 @@ do_one_features = function(name) {
                 kind = "image",
                 src = figfile
             ),
-            h1 = ifelse(is.null(case$section), name, case$section),
-            h2 = ifelse(is.null(case$section), "#", name)
+            h1 = ifelse(is.null(section), name, section),
+            h2 = ifelse(is.null(section), "#", name)
         )
         return(NULL)
     }
