f625a468058d93bf89d1f6145622599058896874
pwwang <pwwang@pwwang.com>
2024-01-15 20:50:03 -0600

    cell_ranger: Use `Iframe` in report to have loading indicators

--- biopipen/reports/cellranger/CellRangerVdj.svelte
+++ biopipen/reports/cellranger/CellRangerVdj.svelte
@@ -1,11 +1,14 @@
 {% from "utils/misc.liq" import report_jobs, table_of_images -%}
+<script>
+    import { Iframe } from "$libs";
+</script>
 
 {%- macro report_job(job, h=1) -%}
-    <iframe
+    <Iframe
         src="{{job.out.outdir}}/outs/web_summary.html"
         width="100%"
         frameborder="0"
-        style="min-height: 60vh"></iframe>
+        style="min-height: 60vh" />
 {%- endmacro -%}
 
 {%- macro head_job(job) -%}
