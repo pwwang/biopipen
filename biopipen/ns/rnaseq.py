f0d7a17e4cee99aa4adae8297a6042e6bdf88882
pwwang <pwwang@pwwang.com>
2024-01-30 11:44:58 -0600

    feat: add rnaseq.Simulation to simulate RNAseq data

--- biopipen/ns/rnaseq.py
+++ biopipen/ns/rnaseq.py
@@ -19,3 +19,41 @@ class UnitConversion(Proc):
         "outlog2p": False,
     }
     script = "file://../scripts/rnaseq/UnitConversion.R"
+
+
+class Simulation(Proc):
+    """Simulate RNA-seq data using ESCO package
+
+    Input:
+        ngenes: Number of genes to simulate
+        nsamples: Number of samples to simulate
+            If you want to force the process to re-simulate for the same
+            `ngenes` and `nsamples`, you can set a different value for `envs.seed`.
+            Note that the samples will be shown as cells in the output (since
+            the simulation is designed for single-cell RNA-seq data).
+
+    Output:
+        outdir: Output directory containing the simulated data
+
+    Envs:
+        ncores (type=int): Number of cores to use.
+        type (choice): Which type of heterogenounity to use.
+            - single: produces a single population.
+            - group: produces distinct groups.
+            - tree: produces distinct groups but admits a tree structure.
+            - traj: produces distinct groups but admits a smooth trajectory structure.
+        seed (type=int): Random seed.
+            If not set, seed will not be set.
+        args (type=json): Additional arguments to pass to the simulation function.
+            See <https://rdrr.io/github/JINJINT/ESCO/man/escoParams.html>.
+    """
+    input = "ngenes:var, nsamples:var"
+    output = "outdir:dir:{{in.ngenes}}x{{in.nsamples}}.sim"
+    lang = config.lang.rscript
+    envs = {
+        "ncores": config.misc.ncores,
+        "type": "single",
+        "args": {"dropout-type": "none"},
+        "seed": None,
+    }
+    script = "file://../scripts/rnaseq/Simulation.R"
