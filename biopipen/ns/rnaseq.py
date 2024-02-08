1f636d9761a8f61e94b7f8c1cf949355d9fd0331
pwwang <pwwang@pwwang.com>
2024-02-08 14:14:49 -0600

    feat: add rnaseq.UnitConversion and tests

--- biopipen/ns/rnaseq.py
+++ biopipen/ns/rnaseq.py
@@ -5,18 +5,75 @@ from ..core.config import config
 
 
 class UnitConversion(Proc):
-    """Convert expression value units back and forth"""
+    """Convert expression value units back and forth
+
+    See <https://haroldpimentel.wordpress.com/2014/05/08/what-the-fpkm-a-review-rna-seq-expression-units/>
+    and <https://docs.gdc.cancer.gov/Data/Bioinformatics_Pipelines/Expression_mRNA_Pipeline/#fpkm>.
+
+    Following converstions are supported -
+    * `count -> cpm, fpkm/rpkm, fpkmuq/rpkmrq, tpm, tmm`
+    * `fpkm/rpkm -> count, tpm, cpm`
+    * `tpm -> count, fpkm/rpkm, cpm`
+    * `cpm -> count, fpkm/rpkm, tpm`
+    NOTE that during some conversions, `sum(counts/effLen)` is approximated to
+    `sum(counts)/sum(effLen) * length(effLen))`
+
+    Input:
+        infile: Input file containing expression values
+            The file should be a matrix with rows representing genes and columns
+            representing samples.
+            It could be an RDS file containing a data frame or a matrix, or a
+            text file containing a matrix with tab as the delimiter. The text
+            file can be gzipped.
+
+    Output:
+        outfile: Output file containing the converted expression values
+            The file will be a matrix with rows representing genes and columns
+            representing samples.
+
+    Envs:
+        inunit: The input unit of the expression values.
+            You can also use an expression to indicate the input unit, e.g.,
+            `log2(counts + 1)`. The expression should be like `A * fn(B*X + C) + D`,
+            where `A`, `B`, `C` and `D` are constants, `fn` is a function, and X is
+            the input unit.
+            Currently only `expr`, `sqrt`, `log2`, `log10` and `log` are supported as
+            functions.
+            Supported input units are:
+            * counts/count/rawcounts/rawcount: raw counts.
+            * cpm: counts per million.
+            * fpkm/rpkm: fragments per kilobase of transcript per million.
+            * fpkmuq/rpkmuq: upper quartile normalized FPKM/RPKM.
+            * tpm: transcripts per million.
+            * tmm: trimmed mean of M-values.
+        outunit: The output unit of the expression values. An expression can also be
+            used for transformation (e.g. `log2(tpm + 1)`). If `inunit` is `count`,
+            then this means we are converting raw counts to tpm, and transforming it
+            to `log2(tpm + 1)` as the output. Any expression supported by `R` can be
+            used. Same units as `inunit` are supported.
+        refexon: Path to the reference exon gff file.
+        meanfl (type=auto): A file containing the mean fragment length for each sample
+            by rows (samples as rowname), without header.
+            Or a fixed universal estimated number (1 used by TCGA).
+        nreads (type=auto): The estimatied total number of reads for each sample.
+            or you can pass a file with the number for each sample by rows
+            (samples as rowname), without header.
+            When converting `fpkm/rpkm -> count`, it should be total reads of that sample.
+            When converting `cpm -> count`: it should be total reads of that sample.
+            When converting `tpm -> count`: it should be total reads of that sample.
+            When converting `tpm -> cpm`: it should be total reads of that sample.
+            When converting `tpm -> fpkm/rpkm`: it should be `sum(fpkm)` of that sample.
+            It is not used when converting `count -> cpm, fpkm/rpkm, tpm`.
+    """  # noqa: E501
     input = "infile:file"
     output = "outfile:file:{{in.infile | basename}}"
     lang = config.lang.rscript
     envs = {
-        "infmt": "matrix",  # or rds
         "inunit": None,
         "outunit": None,
         "refexon": config.ref.refexon,
-        "meanfl": None,
-        "inlog2p": False,
-        "outlog2p": False,
+        "meanfl": 1,
+        "nreads": 1_000_000,
     }
     script = "file://../scripts/rnaseq/UnitConversion.R"
 
@@ -37,15 +94,14 @@ class Simulation(Proc):
             `sim.rds` and `True.rds` will be generated.
             For `ESCO`, `sim.rds` contains the simulated data in a
             `SingleCellExperiment` object, and `True.rds` contains the matrix of true
-            counts with columns
-            representing samples and rows representing genes.
+            counts.
             For `RUVcorr`, `sim.rds` contains the simulated data in list with
             `Truth`, A matrix containing the values of Xβ; `Y` A matrix containing the
             values in `Y`; `Noise` A matrix containing the values in `Wα`; `Sigma`
             A matrix containing the true gene-gene correlations, as defined by Xβ; and
             `Info` A matrix containing some of the general information about the
-            simulation. For the matrices, rows represent genes and columns represent
-            samples.
+            simulation.
+            For all matrices, rows represent genes and columns represent samples.
 
     Envs:
         tool (choice): Which tool to use for simulation.
--- biopipen/scripts/rnaseq/UnitConversion.R
+++ biopipen/scripts/rnaseq/UnitConversion.R
@@ -1,73 +1,338 @@
-infile = {{in.infile | r}}
-outfile = {{out.outfile | r}}
-infmt = {{envs.infmt | r}}
-inunit = {{envs.inunit | r}}
-outunit = {{envs.outunit | r}}
-refexon = {{envs.refexon | r}}
-inlog2p = {{envs.inlog2p | r}}
-outlog2p = {{envs.outlog2p | r}}
+source("{{biopipen_dir}}/utils/misc.R")
+library(rlang)
+library(glue)
 
-if (infmt == "rds") {
-    indata = readRDS(infile)
-} else if (endsWith(infile, ".gz")) {
-    indata = read.table(infile, header=T, row.names=NULL, sep="\t", check.names = F)
-    genes = make.unique(indata[, 1])
-    indata = indata[, -1]
-    rownames(indata) = genes
+infile <- {{in.infile | r}}
+outfile <- {{out.outfile | r}}
+inunit <- {{envs.inunit | r}}
+outunit <- {{envs.outunit | r}}
+refexon <- {{envs.refexon | r}}
+meanfl <- {{envs.meanfl | r}}
+nreads <- {{envs.nreads | r}}
+
+log_info("Reading input data ...")
+indata = read.table(infile, header = TRUE, sep = "\t", row.names = 1, check.names = F)
+samples = colnames(indata)
+
+# parse the inunit to see if there is any transformation
+parsable <- function(arg) {	is.call(arg) || is_symbol(arg) }
+
+check_call_args <- function(arg1, arg2) {
+    if (parsable(arg1) && parsable(arg2)) {
+        stop(glue("Can't parse the call. Multiple names or calls detected: {arg1}, {arg2}\n"))
+    }
+    if (!parsable(arg1) && !parsable(arg2)) {
+        stop(glue("Can't parse the call. Both arguments are constants: {arg1}, {arg2}. Use the result directly\n"))
+    }
 }
 
+parse_call <- function(call, expr = "indata") {
+    if (!is.call(call)) {
+        call <- match.arg(
+            as_string(call),
+            c(
+                "count", "counts", "rawcount", "rawcounts",
+                "cpm",
+                "fpkm", "rpkm",
+                "fpkmuq", "rpkmuq",
+                "tpm",
+                "tmm"
+            )
+        )
+        return(glue("{as_string(call)} = {expr}"))
+    }
+    cn <- as_string(call_name(call))
+    args <- call_args(call)
+    if (length(args) == 1) {
+        # This should be those supported functions
+        cn <- match.arg(cn, c("log", "log2", "log10", "exp", "sqrt"))
+        if (cn == "log") return(parse_call(args[[1]], glue("e ^ ({expr})")))
+        if (cn == "log2") return(parse_call(args[[1]], glue("2 ^ ({expr})")))
+        if (cn == "log10") return(parse_call(args[[1]], glue("10 ^ ({expr})")))
+        if (cn == "exp") return(parse_call(args[[1]], glue("log({expr})")))
+        if (cn == "sqrt") return(parse_call(args[[1]], glue("({expr}) ^ 2")))
+    } else {
+        check_call_args(args[[1]], args[[2]])
+        if (cn == "+") {
+            if (parsable(args[[1]])) return(parse_call(args[[1]], glue("{expr} - {args[[2]]}")))
+            return(parse_call(args[[2]], glue("{expr} - {args[[1]]}")))
+        }
+        if (cn == "-") {
+            if (parsable(args[[1]])) return(parse_call(args[[1]], glue("{expr} + {args[[2]]}")))
+            return(parse_call(args[[2]], glue("{args[[1]]} - {expr}")))
+        }
+        if (cn == "*") {
+            if (parsable(args[[1]])) return(parse_call(args[[1]], glue("({expr}) / ({args[[2]]})")))
+            return(parse_call(args[[2]], glue("({expr}) / ({args[[1]]})")))
+        }
+        if (cn == "/") {
+            if (parsable(args[[1]])) return(parse_call(args[[1]], glue("({expr}) * ({args[[2]]})")))
+            return(parse_call(args[[2]], glue("({args[[1]]}) / ({expr})")))
+        }
+        if (cn == "^") {
+            if (parsable(args[[1]])) return(parse_call(args[[1]], glue("{expr} * (1 / ({args[[2]]}))")))
+            return(parse_call(args[[2]], glue("log({expr}, {args[[1]]})")))
+        }
+        stop(paste0("Unknown function to parse: {cn}\n"))
+    }
+}
 
-glenFromExon = function(exonfile, x) {
-	gff  = read.table(exonfile, header = F, row.names = NULL)
-	# V4: start, V5: end, V10: gene name
-	glen = aggregate(V5-V4+1 ~ V10, gff, sum)
-	genes = glen[,1]
-	glen = glen[,-1,drop=F]
-	rownames(glen) = genes
+glenFromExon = function(exonfile, data) {
+    gff  = read.table(exonfile, header = F, row.names = NULL)
+    # V4: start, V5: end, V10: gene name
+    glen = aggregate(V5-V4+1 ~ V10, gff, sum)
+    genes = glen[,1]
+    glen = glen[,-1,drop=F]
+    rownames(glen) = genes
 
-	mygenes  = rownames(x)
-	outgenes = intersect(genes, mygenes)
-	if (length(outgenes) < length(mygenes))
-		warning('Genes not found in refexon: ', paste(setdiff(mygenes, outgenes)))
+    mygenes  = rownames(data)
+    outgenes = intersect(genes, mygenes)
+    if (length(outgenes) < length(mygenes))
+        logger('Genes not found in refexon: ', paste(setdiff(mygenes, outgenes), collapse = ','), level = 'WARNING')
 
-	glen[outgenes, , drop = FALSE]
+    glen[outgenes, , drop = FALSE]
 }
 
 meanflFromFile = function(samples, mflfile) {
-	if (is.numeric(mflfile)) {
-		ret = matrix(mflfile, nrow = length(samples), ncol = 1)
-		rownames(ret) = samples
-	} else {
-		ret = read.table(mflfile, header = F, row.names = 1, check.names = F, sep = "\t")
-		ret = ret[samples,,drop = F]
-	}
-	ret
+    if (is.numeric(mflfile)) {
+        ret = matrix(mflfile, nrow = length(samples), ncol = 1)
+        rownames(ret) = samples
+    } else {
+        ret = read.table(mflfile, header = F, row.names = 1, check.names = F, sep = "\t")
+        ret = ret[samples,,drop = F]
+    }
+    ret
 }
 
-count2tpm = function(x) {
-	glen = glenFromExon(refexon, x)
-	x = x[rownames(glen), , drop = F]
-	fld  = meanflFromFile(samples, meanfl)
-
-	# see: https://gist.github.com/slowkow/c6ab0348747f86e2748b
-	expr = as.data.frame(sapply(samples, function(s){
-		rate  = log(x[, s]) - log(glen - fld[s, ] + 1)
-		denom = log(sum(exp(rate)))
-		exp(rate - denom + log(1e6))
-	}))
-	colnames(expr) = colnames(x)
-	rownames(expr) = rownames(x)
-	expr
+nreadsFromFile = function(samples, nreads) {
+    if (is.numeric(nreads)) {
+        ret = matrix(nreads, nrow = length(samples), ncol = 1)
+        rownames(ret) = samples
+    } else {
+        ret = read.table(nreads, header = F, row.names = 1, check.names = F, sep = "\t")
+        ret = ret[samples,,drop = F]
+    }
+    ret
 }
 
-if (inunit %in% c('count', 'counts', 'rawcount', 'rawcounts')) {
-    inunit = "count"
+count2cpm <- function(data) {
+    edgeR::cpm(data)
 }
 
+count2fpkm = function(data) {
+    # may lose some genes
+    glen = glenFromExon(refexon, data)
+    data = data[rownames(glen), , drop = F]
+    dge  = edgeR::DGEList(counts=data)
 
-convert = function(data, inunit, outunit) {
-    func = get(paste0(inunit, "2", outunit))
-    func(data)
+    dge$genes$Length = glen
+    edgeR::rpkm(dge)
 }
 
-convert(indata, inunit, outunit)
+count2fpkmuq = function(data) {
+    # may lose some genes
+    glen = glenFromExon(refexon, data)
+    data = data[rownames(glen), , drop = FALSE]
+
+    fld  = meanflFromFile(samples, meanfl)
+    expr = sapply(samples, function(s){
+        RC75 = quantile(data[, s], .75)
+        exp( log(data[, s]) + log(1e9) - log(glen - fld[s, ] + 1) - log(RC75) )
+    })
+    rownames(expr) = rownames(data)
+    expr
+}
+
+count2tpm = function(data) {
+    glen = glenFromExon(refexon, data)
+    data = data[rownames(glen), , drop = F]
+    fld  = meanflFromFile(samples, meanfl)
+
+    # see: https://gist.github.com/slowkow/c6ab0348747f86e2748b
+    expr = as.data.frame(sapply(samples, function(s){
+        rate  = log(data[, s]) - log(glen - fld[s, ] + 1)
+        denom = log(sum(exp(rate)))
+        exp(rate - denom + log(1e6))
+    }))
+    colnames(expr) = colnames(data)
+    rownames(expr) = rownames(data)
+    expr
+}
+
+count2tmm = function(data) {
+    dge = edgeR::DGEList(counts=data)
+    dge = edgeR::calcNormFactors(dge, method = "TMM")
+    edgeR::cpm(dge)
+}
+
+fpkm2count = function(data) {
+    glen    = glenFromExon(refexon, data)
+    data    = data[rownames(glen), , drop = F]
+    fld     = meanflFromFile(samples, meanfl)
+    totalnr = nreadsFromFile(samples, nreads)
+
+    expr    = sapply(samples, function(s){
+        N = totalnr[s, ]
+        exp( log(data[, s]) + log(N) + log(glen - fld[s, ] + 1) - log(1e9) )
+    })
+    rownames(expr) = rownames(data)
+    expr
+}
+
+fpkm2tpm = function(data) {
+    expr = sapply(samples, function(s) {
+        exp( log(data[, s]) - log(sum(data[, s])) + log(1e6) )
+    })
+    rownames(expr) = rownames(data)
+    expr
+}
+
+fpkm2cpm = function(data) {
+    glen = glenFromExon(refexon, data)
+    data = data[rownames(glen), , drop = F]
+    expr = sapply(samples, function(s) {
+        exp( log(data[, s]) - log(1e3) - log(glen - fld[s, ] + 1) )
+    })
+    rownames(expr) = rownames(data)
+    expr
+}
+
+tpm2count = function(data) {
+    totalnr = nreadsFromFile(samples, nreads)
+    ngenes  = nrow(data)
+
+    expr     = sapply(samples, function(s){
+        # counts to tpm:
+        # rate <- log(counts) - log(effLen)
+        # denom <- log(sum(exp(rate)))
+        # tpm = exp(rate - denom + log(1e6))
+        # so:
+        # log(tpm) = rate - denom + log(1e6)
+        # rate = log(tpm) + denom - log(1e6)
+        # log(counts) - log(effLen) = log(tpm) + log(sum(exp(rate))) - log(1e6)
+        # log(counts) - log(effLen) = log(tpm) + log(sum(exp(log(counts) - log(effLen)))) - log(1e6)
+        # log(counts) - log(effLen) = log(tpm) + log(sum(exp(log(counts))/exp(log(effLen)))) - log(1e6)
+        # log(counts) - log(effLen) = log(tpm) + log(sum(counts/effLen)) - log(1e6)
+        #                                                ?????????????
+        # ??? estimated by sum(counts)/sum(effLen) * length(effLen)
+        # log(counts) = log(effLen) + log(tpm) + log(sum(counts)) - log(effLen) + log(length(effLen))) - log(1e6)
+        # counts = expr( log(tpm) + log(nreads) + log(length(effLen)) - log(1e6) )
+        exp( log(data[, s]) + log(totalnr[s, ]) + log(ngenes) - log(1e6) )
+    })
+    rownames(expr) = rownames(data)
+    expr
+}
+
+tpm2fpkm = function(data) {
+    totalnr = nreadsFromFile(samples, nreads)
+    expr = sapply(samples, function(s) {
+        exp( log(data[, s]) - log(1e6) + log(totalnr[s, ]) )
+    })
+    rownames(expr) = rownames(data)
+    expr
+}
+
+tpm2cpm = function(data) {
+    glen   = glenFromExon(refexon, data)
+    data   = data[rownames(glen), , drop = F]
+    fld    = meanflFromFile(samples, meanfl)
+    ngenes = length(outgenes)
+
+    expr = sapply(samples, function(s) {
+        exp( log(data[, s]) + log(glen - fld[s, ] + 1) - log(sum(glen - fld[s, ] + 1)) + log(ngenes) )
+    })
+    rownames(expr) = rownames(data)
+    expr
+}
+
+cpm2count = function(data) {
+    totalnr = nreadsFromFile(samples, nreads)
+
+    expr = sapply(samples, function(s) {
+        exp( log(data[, s]) + log(totalnr[s, ]) - log(1e6) )
+    })
+    rownames(expr) = rownames(data)
+    expr
+}
+
+cpm2fpkm = function(data) {
+    glen = glenFromExon(refexon, data)
+    data = data[rownames(glen), , drop = F]
+    expr = sapply(samples, function(s) {
+        exp( log(data[, s]) + log(1e3) - log(glen - fld[s, ] + 1) )
+    })
+    rownames(expr) = rownames(data)
+    expr
+}
+
+cpm2tpm = function(data) {
+    glen   = glenFromExon(refexon, data)
+    data   = data[rownames(glen), , drop = F]
+    ngenes = nrow(glen)
+    expr   = sapply(samples, function(s) {
+        exp( log(data[, s]) - log(glen - fld[s, ] + 1) - log(sum(glen - fld[s, ] + 1)) + log(ngenes) )
+    })
+    rownames(expr) = rownames(data)
+    expr
+}
+
+is.count  = function(unit) {unit %in% c('count', 'counts', 'rawcount', 'rawcounts')}
+is.cpm    = function(unit) {unit == 'cpm'}
+is.fpkm   = function(unit) {unit %in% c('fpkm', 'rpkm')}
+is.fpkmuq = function(unit) {unit %in% c('fpkmuq', 'rpkmuq')}
+is.tpm    = function(unit) {unit == 'tpm'}
+is.tmm    = function(unit) {unit == 'tmm'}
+
+# log2(count + 1) -> count = 2 ^ indata - 1
+parsed_transformation <- parse_call(parse_expr(inunit))
+splits <- strsplit(parsed_transformation, " = ")[[1]]
+if (is.count(splits[[1]])) {
+    intype <- "count"
+} else if (is.cpm(splits[[1]])) {
+    intype <- "cpm"
+} else if (is.fpkm(splits[[1]])) {
+    intype <- "fpkm"
+} else if (is.fpkmuq(splits[[1]])) {
+    intype <- "fpkmuq"
+} else if (is.tpm(splits[[1]])) {
+    intype <- "tpm"
+} else if (is.tmm(splits[[1]])) {
+    intype <- "tmm"
+} else {
+    stop(glue("Can't find a supported unit in the inunit: {inunit}\n"))
+}
+splits[1] <- intype
+eval(parse_expr(paste(splits, collapse = " = ")))
+indata <- get(intype)
+
+# find out the outtype
+if (grepl('rawcounts|rawcount|counts|count', outunit)) {
+    outtype <- 'count'
+    outunit <- gsub('rawcounts|rawcount|counts|count', 'count', outunit)
+} else if (grepl('fpkmuq|rpkmuq', outunit)) {
+    outtype <- 'fpkmuq'
+    outunit <- gsub('fpkmuq|rpkmuq', 'fpkmuq', outunit)
+} else if (grepl('fpkm|rpkm', outunit)) {
+    outtype <- 'fpkm'
+    outunit <- gsub('fpkm|rpkm', 'fpkm', outunit)
+} else if (grepl('tpm', outunit)) {
+    outtype <- 'tpm'
+} else if (grepl('cpm', outunit)) {
+    outtype <- 'cpm'
+} else if (grepl('tmm', outunit)) {
+    outtype <- 'tmm'
+} else {
+    stop(glue("Can't find a supported unit in the outunit: {outunit}\n"))
+}
+
+log_info("Transforming data by resolving {inunit} ...")
+fun <- glue("{intype}2{outtype}")
+fun <- tryCatch(
+    { get(fun) },
+    error = function(e) { stop(glue("Unsupported conversion from {intype} to {outunit}\n")) }
+)
+assign(outtype, fun(indata))
+out <- eval(parse_expr(outunit))
+
+log_info("Saving output data ...")
+write.table(out, outfile, quote=FALSE, row.names=TRUE, col.names=TRUE, sep="\t")
