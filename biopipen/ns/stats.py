4fb744fbe27c080ba7814fc6adfc8af3623c903d
pwwang <pwwang@pwwang.com>
2024-02-20 10:16:46 -0600

    feat: add stats.LiquidAssoc

--- biopipen/ns/stats.py
+++ biopipen/ns/stats.py
@@ -73,3 +73,107 @@ class ChowTest(Proc):
     script = "file://../scripts/stats/ChowTest.R"
 
 
+class LiquidAssoc(Proc):
+    """Liquid association tests.
+
+    See Also https://github.com/gundt/fastLiquidAssociation
+    Requieres https://github.com/pwwang/fastLiquidAssociation
+
+    Input:
+        infile: The input data file. The rows are samples and the columns are
+            features. It must be tab-delimited.
+            ```
+            Sample   F1   F2   F3   ...   Fn
+            S1       1.2  3.4  5.6        7.8
+            S2       2.3  4.5  6.7        8.9
+            ...
+            Sm       5.6  7.8  9.0        1.2
+            ```
+            The features (columns) will be tested pairwise, which will be the X and
+            Y columns in the result of `fastMLA`
+        covfile: The covariate file. The rows are the samples and the columns
+            are the covariates. It must be tab-delimited.
+            If provided, the data in `in.infile` will be adjusted by covariates by
+            regressing out the covariates and the residuals will be used for
+            liquid association tests.
+        groupfile: The group file. The rows are the samples and the columns
+            are the groupings. It must be tab-delimited.
+            ```
+            Sample   G1   G2   G3   ...   Gk
+            S1       0    1    0          0
+            S2       2    1    0          NA  # exclude this sample
+            ...
+            Sm       1    0    0          0
+            ```
+            This will be served as the Z column in the result of `fastMLA`
+            This can be omitted. If so, `envs.nvec` should be specified, which is
+            to select column from `in.infile` as Z.
+        fmlfile: The formula file. The 3 columns are X3, X12 and X21. The results
+            will be filtered based on the formula. It must be tab-delimited without
+            header.
+
+    Output:
+        outfile: The output file.
+            ```
+            X12  X21  X3  rhodiff  MLA value  estimates  san.se  wald  Pval  model
+            C38  C46  C5  0.87  0.32  0.67  0.20  10.87  0  F
+            C46  C38  C5  0.87  0.32  0.67  0.20  10.87  0  F
+            C27  C39  C4  0.94  0.34  1.22  0.38  10.03  0  F
+            ```
+
+    Envs:
+        nvec: The column index (1-based) of Z in `in.infile`, if `in.groupfile` is
+            omitted. You can specify multiple columns by comma-seperated values, or
+            a range of columns by `-`. For example, `1,3,5-7,9`. It also supports
+            column names. For example, `F1,F3`. `-` is not supported for column
+            names.
+        x: Similar as `nvec`, but limit X group to given features.
+            The rest of features (other than X and Z) in `in.infile` will
+            be used as Y.
+            The features in `in.infile` will still be tested pairwise, but only
+            features in X and Y will be kept.
+        topn (type=int): Number of results to return by `fastMLA`, ordered from
+            highest `|MLA|` value descending.
+            The default of the package is 2000, but here we set to 1e6 to return as
+            many results as possible (also good to do pvalue adjustment).
+        rvalue (type=float): Tolerance value for LA approximation. Lower values of
+            rvalue will cause a more thorough search, but take longer.
+        cut (type=int): Value passed to the GLA function to create buckets
+            (equal to number of buckets+1). Values placing between 15-30 samples per
+            bucket are optimal. Must be a positive integer>1. By default,
+            `max(ceiling(nrow(data)/22), 4)` is used.
+        ncores (type=int): Number of cores to use for parallelization.
+        padj (choice): The method for p-value adjustment.
+            - none: No p-value adjustment (no Padj column in outfile).
+            - holm: Holm-Bonferroni method.
+            - hochberg: Hochberg method.
+            - hommel: Hommel method.
+            - bonferroni: Bonferroni method.
+            - BH: Benjamini-Hochberg method.
+            - BY: Benjamini-Yekutieli method.
+            - fdr: FDR correction method.
+        transpose_input (flag): Whether to transpose the input file.
+        transpose_group (flag): Whether to transpose the group file.
+        transpose_cov (flag): Whether to transpose the covariate file.
+        xyz_names: The names of X12, X21 and X3 in the final output file. Separated
+            by comma. For example, `X12,X21,X3`.
+    """
+    input = "infile:file, covfile:file, groupfile:file, fmlfile:file"
+    output = "outfile:file:{{in.infile | stem}}.liquidassoc.txt"
+    lang = config.lang.rscript
+    envs = {
+        "nvec": None,
+        "x": None,
+        "topn": 1e6,
+        "rvalue": 0.5,
+        "cut": 20,
+        "ncores": config.misc.ncores,
+        "padj": "none",
+        "transpose_input": False,
+        "transpose_group": False,
+        "transpose_cov": False,
+        "xyz_names": None,
+    }
+    script = "file://../scripts/stats/LiquidAssoc.R"
+
+
---
+++ biopipen/scripts/stats/LiquidAssoc.R
@@ -0,0 +1,136 @@
+source("{{biopipen_dir}}/utils/misc.R")
+
+library(rlang)
+library(dplyr)
+library(tidyr)
+library(fastLiquidAssociation)
+
+infile <- {{in.infile | r}}
+covfile <- {{in.covfile | r}}
+groupfile <- {{in.groupfile | r}}
+fmlfile <- {{in.fmlfile | r}}
+outfile <- {{out.outfile | r}}
+x <- {{envs.x | r}}
+nvec <- {{envs.nvec | r}}
+topn <- {{envs.topn | r}}
+rvalue <- {{envs.rvalue | r}}
+cut <- {{envs.cut | r}}
+ncores <- {{envs.ncores | r}}
+padj <- {{envs.padj | r}}
+transpose_input <- {{envs.transpose_input | r}}
+transpose_group <- {{envs.transpose_group | r}}
+transpose_cov <- {{envs.transpose_cov | r}}
+xyz_names <- {{envs.xyz_names | r}}
+if (!is.null(xyz_names) && length(xyz_names) == 1) {
+	xyz_names <- trimws(strsplit(xyz_names, ",")[[1]])
+}
+
+if (is.null(groupfile) && is.null(nvec)) {
+	stop("Must provide either in.groupfile or envs.nvec")
+}
+if (!is.null(groupfile) && !is.null(nvec)) {
+	stop("Must provide either in.groupfile or envs.nvec, not both")
+}
+
+log_info("Reading and preparing data ...")
+indata <- read.table(infile, header = TRUE, sep = "\t", row.names = 1, check.names = FALSE)
+if (transpose_input) {
+	indata <- t(indata)
+}
+if (!is.null(covfile)) {
+	covdata <- read.table(covfile, header = TRUE, sep = "\t", row.names = 1)
+	if (transpose_cov) {
+		covdata <- t(covdata)
+	}
+	if (!isTRUE(all.equal(rownames(indata), rownames(covdata)))) {
+		stop("Row names of indata and covdata must be identical")
+	}
+	indata <- indata %>% mutate(across(everything(), function(xx) {
+		lm(xx ~ as.matrix(covdata))$residuals
+	}))
+}
+
+expand_range <- function(range) {
+	items <- trimws(strsplit(range, ",|-")[[1]])
+	num_items <- as.numeric(items)
+	if (anyNA(num_items)) {
+		# it's sample names
+		return(match(items, colnames(indata)))
+	}
+	return(num_items)
+}
+
+cut <- cut %||% max(ceiling(nrow(indata)/22), 4)
+if (!is.null(x)) { x <- expand_range(x) }
+if (!is.null(groupfile)) {
+	groupdata <- read.table(groupfile, header = TRUE, sep = "\t", row.names = 1)
+	if (transpose_group) {
+		groupdata <- t(groupdata)
+	}
+	if (!isTRUE(all.equal(rownames(indata), rownames(groupdata)))) {
+		stop("Row names of indata and groupdata must be identical")
+	}
+	nvec <- (ncol(indata) + 1) : (ncol(indata) + ncol(groupdata))
+	indata <- cbind(indata, groupdata)
+} else {
+	nvec <- expand_range(nvec)
+}
+
+log_info("Running fastLiquidAssociation ...")
+indata <- as.matrix(indata)
+mla <- fastMLA(
+	data = indata,
+	topn = topn,
+	rvalue = rvalue,
+	cut = cut,
+	threads = ncores,
+	nvec = nvec
+)
+
+if (nrow(mla) == 0) {
+	log_warn("No significant associations found")
+	out <- data.frame(
+		X12 = character(),
+		X21 = character(),
+		X3 = character(),
+		rhodiff = numeric(),
+		`MLA.value` = numeric(),
+		estimates = numeric(),
+		`san.se` = numeric(),
+		wald = numeric(),
+		Pval = numeric(),
+		model = character()
+	)
+} else {
+	cnm <- mass.CNM(data = indata, GLA.mat = mla, nback = topn)
+	out <- cnm$`top p-values` %>%
+		dplyr::select(X12 = "X1 or X2", X21 = "X2 or X1", everything(), Pval = "p value")
+}
+
+if (!is.null(fmlfile)) {
+	fmldata <- read.table(fmlfile, header = FALSE, sep = "\t", row.names = NULL)
+	colnames(fmldata) <- c("Z", "X", "Y")
+	all_combns <- fmldata %>% unite("XYZ", X, Y, Z, sep = " // ") %>% pull(XYZ)
+	out <- out %>%
+		unite("XYZ", X12, X21, X3, sep = " // ", remove = FALSE) %>%
+		dplyr::filter(XYZ %in% all_combns) %>%
+		dplyr::select(-XYZ)
+}
+
+if (!is.null(xyz_names)) {
+	out <- out %>%
+		dplyr::select(
+			!!sym(xyz_names[1]) := "X12",
+			!!sym(xyz_names[2]) := "X21",
+			!!sym(xyz_names[3]) := "X3",
+			everything()
+		)
+}
+
+if (padj != "none") {
+	log_info("Calculating adjusted p-values ...")
+	out$Padj <- p.adjust(out$Pval, method = padj)
+}
+
+log_info("Writing output ...")
+write.table(out, file = outfile, sep = "\t", quote = FALSE, row.names = FALSE)
