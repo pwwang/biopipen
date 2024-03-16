88bda9f0dd442955d9c8722fc9fb37f45bda034a
pwwang <pwwang@pwwang.com>
2024-03-15 23:51:40 -0500

    tests: refactor tests for utils.gene using unittest

--- tests/test_utils/gene/test.py
+++ tests/test_utils/gene/test.py
@@ -1,44 +1,33 @@
-# TODO: use unittest
+from unittest import TestCase, main
 from biopipen.utils.gene import gene_name_conversion
 
 
-def run_except(genes, infmt, outfmt, notfound, expect, species):
-    try:
-        gene_name_conversion(genes, species, infmt, outfmt, notfound)
-    except expect:
-        pass
-    else:
-        raise ValueError("Test Failed!")
+class TestGene(TestCase):
 
+    def test_gene_name_conversion1(self):
+        # gene_name_conversion(genes, species, infmt, outfmt, notfound)
+        out = gene_name_conversion(
+            ['1255_g_at', '1316_at', '1320_at'],
+            "human",
+            "reporter",
+            "symbol",
+            "ignore"
+        )
+        self.assertListEqual(out["symbol"].tolist(), ["GUCA1A", "THRA", "PTPN21"])
 
-def run_expect(genes, infmt, outfmt, notfound, expect, species):
-    out = gene_name_conversion(genes, species, infmt, outfmt, notfound)
-    assert out[outfmt].tolist() == expect
-
-
-def run(genes, infmt, outfmt, notfound, expect, species="human"):
-    print(f">>> TESTING {genes} - {infmt} - {outfmt} - {notfound} ")
-    if isinstance(expect, Exception):
-        run_except(genes, infmt, outfmt, notfound, expect, species)
-
-    else:
-        run_expect(genes, infmt, outfmt, notfound, expect, species)
-    print(">>> PASSED")
-    print(">>> ")
+    def test_gene_name_conversion2(self):
+        out = gene_name_conversion(
+            ['NM_003466', 'CDK2', "695", '1320_at', 'Q08345'],
+            "human",
+            "refseq,symbol,entrezgene,reporter,uniprot",
+            "symbol",
+            "ignore"
+        )
+        self.assertListEqual(
+            out["symbol"].tolist(),
+            ["PAX8", "CDK2", "BTK", "PTPN21", "DDR1"]
+        )
 
 
 if __name__ == "__main__":
-    run(
-        ['1255_g_at', '1316_at', '1320_at'],
-        infmt="reporter",
-        outfmt="symbol",
-        notfound="ignore",
-        expect=["GUCA1A", "THRA", "PTPN21"]
-    )
-    run(
-        ['NM_003466', 'CDK2', "695", '1320_at', 'Q08345'],
-        infmt="refseq,symbol,entrezgene,reporter,uniprot",
-        outfmt="symbol",
-        notfound="ignore",
-        expect=["PAX8", "CDK2", "BTK", "PTPN21", "DDR1"]
-    )
+    main(verbosity=2)
