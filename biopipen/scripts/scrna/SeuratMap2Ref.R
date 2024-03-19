339cac4b673bc054e41f71c78640d1c0628201ac
pwwang <pwwang@pwwang.com>
2024-03-19 01:41:25 -0500

    enh(scrna.SeuratMap2Ref): rename `envs.name` to `envs.ident` and `envs.MapQuery.refdata` is not required anymore.
    - `envs.MapQuery.refdata` will be inferred from `envs.ident` and `envs.use`.

--- biopipen/scripts/scrna/SeuratMap2Ref.R
+++ biopipen/scripts/scrna/SeuratMap2Ref.R
@@ -10,7 +10,7 @@ set.seed(8525)
 sobjfile = {{in.sobjfile | r}}
 outfile = {{out.outfile | r}}
 use = {{envs.use | r}}
-name = {{envs.name | r}}
+ident = {{envs.ident | r}}
 ref = {{envs.ref | r}}
 ncores = {{envs.ncores | r}}
 sctransform_args = {{envs.SCTransform | r: todot="-"}}
@@ -18,22 +18,20 @@ findtransferanchors_args = {{envs.FindTransferAnchors | r: todot="-"}}
 mappingscore_args = {{envs.MappingScore | r: todot="-"}}
 mapquery_args = {{envs.MapQuery | r: todot="-"}}
 
-if (is.null(mapquery_args$refdata) || length(mapquery_args$refdata) == 0) {
-    stop("No refdata provided for MapQuery (envs.MapQuery.refdata)")
-}
-
 # See if we have a reference
 if (is.null(ref)) {
     stop("No reference provided (envs.ref)")
 }
 
 if (is.null(use)) {
-    if (length(mapquery_args$refdata) == 1) {
-        use = paste0("predicted.", names(mapquery_args$refdata)[1])
-    } else {
-        stop("No use provided (envs.use), don't know which column to use as cluster")
-    }
+    stop("No use provided (envs.use), don't know which column to transfer as cluster")
 }
+
+if (is.null(mapquery_args$refdata) || length(mapquery_args$refdata) == 0) {
+    mapquery_args$refdata = list()
+}
+
+mapquery_args$refdata[[use]] = use
 
 outdir = dirname(outfile)
 options(future.globals.maxSize = 80000 * 1024^2)
@@ -49,7 +47,7 @@ plan(strategy = "multicore", workers = ncores)
 findtransferanchors_args = .expand_dims(findtransferanchors_args)
 
 # Load reference
-log_info("Loading reference")
+log_info("- Loading reference")
 if (endsWith(ref, ".rds") || endsWith(ref, ".RDS")) {
     reference = readRDS(ref)
 } else if (endsWith(ref, ".h5ad") || endsWith(ref, ".H5AD")) {
@@ -59,30 +57,30 @@ if (endsWith(ref, ".rds") || endsWith(ref, ".RDS")) {
 }
 
 # Load Seurat object
-log_info("Loading Seurat object")
+log_info("- Loading Seurat object")
 sobj = readRDS(sobjfile)
 
 # Normalize data
-log_info("Normalizing data")
+log_info("- Normalizing data")
 sctransform_args$object = sobj
 sctransform_args$residual.features = rownames(x = reference)
 query = do_call(SCTransform, sctransform_args)
 
 # Find anchors between query and reference
-log_info("Finding anchors")
+log_info("- Finding anchors")
 findtransferanchors_args$reference = reference
 findtransferanchors_args$query = query
 anchors = do_call(FindTransferAnchors, findtransferanchors_args)
 
 # Map query to reference
-log_info("Mapping query to reference")
+log_info("- Mapping query to reference")
 mapquery_args$reference = reference
 mapquery_args$query = query
 mapquery_args$anchorset = anchors
 query = do_call(MapQuery, mapquery_args)
 
 # Calculating mapping score
-log_info("Calculating mapping score")
+log_info("- Calculating mapping score")
 mappingscore_args$anchors = anchors
 mappingscore = tryCatch({
     do_call(MappingScore, mappingscore_args)
@@ -98,7 +96,7 @@ mappingscore = tryCatch({
 })
 
 # Calculate mapping score and add to metadata
-log_info("Calculating mapping score")
+log_info("- Calculating mapping score")
 query = AddMetaData(
   object = query,
   metadata = mappingscore,
@@ -106,12 +104,14 @@ query = AddMetaData(
 )
 
 # Add the alias to the metadata for the clusters
-log_info("Adding name to metadata and set as ident")
-query@meta.data = query@meta.data %>% mutate(!!sym(name) := as.factor(!!parse_expr(use)))
-Idents(query) = name
+log_info("- Adding ident to metadata and set as ident")
+query@meta.data = query@meta.data %>% mutate(
+    !!sym(ident) := as.factor(!!parse_expr(paste0("predicted.", use)))
+)
+Idents(query) = ident
 
 # Save
-log_info("Saving")
+log_info("- Saving result ...")
 saveRDS(query, file = outfile)
 
 
@@ -120,36 +120,37 @@ saveRDS(query, file = outfile)
 # ############################
 
 # # Plot the UMAP
-log_info("Plotting")
-for (refname in names(mapquery_args$refdata)) {
-    if (refname == "predicted_ADT") {
+log_info("- Plotting for transferred data ...")
+ref.reduction = mapquery_args$reduction.model %||% "wnn.umap"
+for (qname in names(mapquery_args$refdata)) {
+    rname <- mapquery_args$refdata[[qname]]
+
+    if (grepl("Array", class(reference[[rname]])) && grepl("Array", class(query[[qname]]))) {
+        log_warn("  Skipping transferred array: {qname} -> {rname}")
         next
     }
-    reduction = if (is.null(mapquery_args$reduction.model)) "wnn.umap" else mapquery_args$reduction.model
-    print(paste0("Plotting UMAP for ", refname))
-    p = DimPlot(
+
+    log_info("  Plotting transferred data: {qname} -> {rname}")
+
+    ref_p <- DimPlot(
         object = reference,
-        reduction = reduction,
-        group.by = refname,
+        reduction = ref.reduction,
+        group.by = rname,
         label = TRUE,
         label.size = 3,
         repel = TRUE,
     ) + NoLegend()
 
-    png(file.path(outdir, paste0("Reference_UMAP_", refname, ".png")), width = 1000, height = 1000, res = 100)
-    print(p)
-    dev.off()
-
-    p = DimPlot(
+    query_p <- DimPlot(
         object = query,
         reduction = "ref.umap",
-        group.by = paste0("predicted.", refname),
+        group.by = paste0("predicted.", qname),
         label = TRUE,
         label.size = 3,
         repel = TRUE,
     ) + NoLegend()
 
-    png(file.path(outdir, paste0("Query_UMAP_", refname, ".png")), width = 1000, height = 1000, res = 100)
-    print(p)
+    png(file.path(outdir, paste0("UMAPs.png")), width = 1400, height = 700, res = 100)
+    print(ref_p | query_p)
     dev.off()
 }
---
+++ tests/test_scrna/SeuratMap2Ref/test.py
@@ -0,0 +1,74 @@
+from pathlib import Path
+
+from biopipen.core.proc import Proc
+from biopipen.core.config import config
+from biopipen.ns.scrna import (
+    SeuratMap2Ref,
+    SeuratClusterStats,
+)
+from biopipen.core.testing import get_pipeline
+
+
+class PrepareQuery(Proc):
+    """Prepare the query data"""
+    input = "name"
+    input_data = ["pbmc3k"]
+    output = "outfile:file:{{in.name}}.RDS"
+    lang = config.lang.rscript
+    script = """
+        library(Seurat)
+        library(SeuratData)
+        name <- {{in.name | r}}
+        InstallData(name)
+        data <- LoadData(name)
+        data <- UpdateSeuratObject(data)
+        saveRDS(data, {{out.outfile | quote}})
+    """
+
+
+class SeuratMap2Ref(SeuratMap2Ref):
+    requires = PrepareQuery
+    envs = {
+        "use": "celltype.l2",
+        "ref": str(
+            Path(__file__).parent.parent.parent
+            / "data"
+            / "reference"
+            / "pbmc_multimodal_2023.rds"
+        ),
+    }
+
+
+class SeuratClusterStats(SeuratClusterStats):
+    requires = SeuratMap2Ref
+    envs = {
+        "stats": {
+            "Number of cells in each cluster by Sample": {
+                "group-by": "seurat_annotations",
+            }
+        }
+    }
+
+
+def pipeline():
+    return get_pipeline(__file__).set_starts(PrepareQuery)
+
+
+def testing(pipen):
+    assert pipen._succeeded
+    outfile = (
+        pipen.procs[-1].workdir.joinpath(
+            "0",
+            "output",
+            "pbmc3k.cluster_stats",
+            "dimplots",
+            "Dimensional-reduction-plot.dim.png",
+        )
+    )
+    assert outfile.is_file(), str(outfile)
+
+
+if __name__ == "__main__":
+    pipen = pipeline()
+    pipen.run()
+    testing(pipen)
