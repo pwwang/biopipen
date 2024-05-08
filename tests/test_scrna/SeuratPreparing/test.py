d43d4921dfe20c4661253fa1a29864ba326cb2ab
pwwang <pwwang@pwwang.com>
2024-05-08 16:44:32 -0500

    test: add tests to scrna.SeuratTo10X and scrna.SeuratPreparing

---
+++ tests/test_scrna/SeuratPreparing/test.py
@@ -0,0 +1,68 @@
+from pathlib import Path
+
+from pipen import Proc
+from biopipen.core.config import config
+from biopipen.ns.scrna import (
+    SeuratTo10X as SeuratTo10X_,
+    SeuratPreparing as SeuratPreparing_,
+)
+from biopipen.core.testing import get_pipeline, _find_testing_index, TESTING_DIR
+
+
+class PrepareSeurat(Proc):
+    """Prepare the data"""
+    input = "name"
+    input_data = ["pbmc3k"]
+    output = "outfile:file:{{in.name}}.RDS"
+    lang = config.lang.rscript
+    script = """
+        library(SeuratData)
+        InstallData("pbmc3k")
+        pbmc3k <- Seurat::UpdateSeuratObject(pbmc3k)
+        pbmc3k$Sample <- paste0("S", sample(1:2, nrow(pbmc3k), replace = TRUE))
+        saveRDS(pbmc3k, {{out.outfile | quote}})
+    """
+
+
+class SeuratTo10X(SeuratTo10X_):
+    requires = PrepareSeurat
+    envs = {"split_by": "Sample"}
+
+
+def gen_input(ch):
+    metafile = Path(TESTING_DIR % {"index": _find_testing_index(False)}).joinpath(
+        "SeuratPreparing-test.txt"
+    )
+    if not metafile.is_file():
+        with metafile.open("w") as f:
+            f.write("Sample\tRNAData\n")
+            for s in Path(ch.iloc[0, 0]).glob("*"):
+                f.write(f"{s.name}\t{s}\n")
+
+    return [metafile]
+
+
+class SeuratPreparing(SeuratPreparing_):
+    requires = SeuratTo10X
+    input_data = gen_input
+    envs = {"cell_qc": "runif(n()) < 0.5"}
+
+
+class SeuratPreparing2(SeuratPreparing_):
+    requires = SeuratTo10X
+    input_data = gen_input
+    envs = {"cell_qc": "runif(n()) < 0.5", "cell_qc_per_sample": True}
+
+
+def pipeline():
+    return get_pipeline(__file__).set_starts(PrepareSeurat)
+
+
+def testing(pipen):
+    assert pipen._succeeded
+
+
+if __name__ == "__main__":
+    pipen = pipeline()
+    pipen.run()
+    testing(pipen)
