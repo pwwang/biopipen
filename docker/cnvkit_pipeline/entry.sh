3eb89c69d1b47620ca6f356636ab7c86494de6b4
pwwang <pwwang@pwwang.com>
2024-05-20 10:57:04 -0500

    chore(cnvkit_pipeline): update entry.sh for running via pipen-board in container

--- docker/cnvkit_pipeline/entry.sh
+++ docker/cnvkit_pipeline/entry.sh
@@ -7,7 +7,7 @@ if [ $# -eq 0 ]; then
     echo "   docker/singularity run <options> pipen run cnvkit_pipeline CNVkitPipeline @<configfile> [options]"
     echo "2. Run the pipeline using pipen-board:"
     echo "   docker run <options> pipen board biopipen/cnvkit-pipeline:dev -a /workdir/board.toml [options]"
-    echo "   singularity run <options> pipen board docker://biopipen/cnvkit-pipeline:dev -a /workdir/board.toml [options]"
+    echo "   singularity run <options> pipen board docker://biopipen/cnvkit-pipeline:dev biopipen.ns.cnvkit_pipeline:CNVkitPipeline -a /workdir/board.toml [options]"
     exit 1
 else
     # Run the command passed from the command line
