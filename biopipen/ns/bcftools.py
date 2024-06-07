a5d4fc11c0da2c699764e83b206e2701c9fb0a01
pwwang <pwwang@pwwang.com>
2024-06-07 17:00:01 -0500

    feat(bcftools.BcftoolsSort): allow sort contigs based on a chrom size file

--- biopipen/ns/bcftools.py
+++ biopipen/ns/bcftools.py
@@ -79,7 +79,13 @@ class BcftoolsFilter(Proc):
 
 
 class BcftoolsSort(Proc):
-    """Sort VCF files
+    """Sort VCF files using `bcftools sort`.
+
+    `bcftools sort` is used to sort VCF files by chromosome and position based on the
+    order of contigs in the header.
+
+    Here we provide a chrsize file to first sort the contigs in the header and then
+    sort the VCF file using `bcftools sort`.
 
     Input:
         infile: The input VCF file
@@ -89,24 +95,38 @@ class BcftoolsSort(Proc):
 
     Envs:
         bcftools: Path to bcftools
-        gz: Whether to gzip the output file
-        index: Whether to index the output file (tbi) (`envs.gz` forced to True)
-        tmpdir: Path to save the intermediate files
-        args: Other arguments for `bcftools sort`. For example `max-mem`.
-            See also https://samtools.github.io/bcftools/bcftools.html#sort
+        tabix: Path to tabix, used to index infile/outfile
+        ncores (type=int): Number of cores (`--threads`) to use
+        gz (flag): Whether to gzip the output file
+        index (flag): Whether to index the output file (tbi) (`envs.gz` forced to True)
+        chrsize: The chromosome size file, from which the chromosome order is used
+            to sort the contig in the header first.
+            If not provided, `bcftools sort` will be used directly.
+        notfound (choice): What if the contig in the VCF file is not found in the
+            `chrsize` file.
+            - error: Report error
+            - remove: Remove the contig from the header.
+                Note that if there are records with the removed contig, an error will
+                be raised by `bcftools sort`
+            - start: Move the contig to the start of the contigs from `chrsize`
+            - end: Move the contig to the end of the contigs from `chrsize`
+        <more>: Other arguments for `bcftools sort`. For example `max_mem`.
+            See also <https://samtools.github.io/bcftools/bcftools.html#sort>
     """
     input = "infile:file"
     output = (
-        "outfile:file:{{in.infile | stem0}}.vcf"
-        "{% if envs.gz or envs.index %}.gz{% endif %}"
+        "outfile:file:{{in.infile | stem: 'gz'}}.vcf"
+        "{{'.gz' if envs.index or envs.gz else ''}}"
     )
     lang = config.lang.python
     envs = {
         "bcftools": config.exe.bcftools,
+        "tabix": config.exe.tabix,
+        "ncores": config.misc.ncores,
+        "chrsize": config.ref.chrsize,
+        "notfound": "remove",
         "gz": True,
         "index": True,
-        "tmpdir": config.path.tmpdir,
-        "args": {},
     }
     script = "file://../scripts/bcftools/BcftoolsSort.py"
 
