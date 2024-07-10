d430a09d73bec0409b8c1c68e25c96d5e6436902
pwwang <pwwang@pwwang.com>
2024-07-10 14:25:11 -0500

    feat(web): add GCloudStorageDownloadFile and GCloudStorageDownloadBucket to download files from GCP

---
+++ biopipen/scripts/web/GCloudStorageDownloadFile.py
@@ -0,0 +1,23 @@
+from biopipen.utils.misc import run_command, dict_to_cli_args
+from biopipen.scripts.web.gcloud_common import is_logged_in, is_valid_gs_file_url
+
+url = {{in.url | repr}}  # pyright: ignore  # noqa: E999
+outfile = {{out.outfile | repr}}  # pyright: ignore
+gcloud = {{envs.gcloud | repr}}  # pyright: ignore
+args = {{envs.args | repr}}  # pyright: ignore
+
+if not is_valid_gs_file_url(url):
+    raise Exception(
+        f"Invalid Google Cloud Storage URL for a file: {url}. "
+        "URL should be in the format gs://bucket/path/to/file"
+    )
+
+if not is_logged_in(gcloud):
+    raise Exception(
+        "You need to be logged in to gcloud to download files. "
+        "Please run `gcloud auth login` first."
+    )
+
+args[""] = [gcloud, "storage", "cp", url, outfile]
+
+run_command(dict_to_cli_args(args, dashify=True), fg=True)
---
+++ biopipen/scripts/web/gcloud_common.py
@@ -0,0 +1,49 @@
+"""Provides common functions for interacting with Google Cloud Storage."""
+from biopipen.utils.misc import run_command
+
+
+def is_logged_in(gcloud: str) -> bool:
+    """Check if the user is logged in to Google Cloud Storage.
+
+    Args:
+        gcloud: Path to the `gcloud` executable.
+
+    Returns:
+        bool: True if the user is logged in, False otherwise.
+    """
+    out = run_command([gcloud, "auth", "list"], stdout="RETURN")
+    return "ACTIVE" in out
+
+
+def is_valid_gs_bucket_url(url: str) -> bool:
+    """Check if a URL is a valid Google Cloud Storage bucket URL.
+
+    Such as `gs://bucket`.
+    """
+    if not url.startswith("gs://"):
+        return False
+
+    url = url.rstrip("/")
+    return "/" not in url[5:]
+
+
+def get_file_path(url: str) -> str:
+    """Get the file path from a Google Cloud Storage file URL, without bucket.
+
+    For example: gs://bucket/path/to/file -> path/to/file
+
+    Args:
+        url: The Google Cloud Storage file URL.
+
+    Returns:
+        str: The file path.
+    """
+    return url[5:].split("/", 1)[1]
+
+
+def is_valid_gs_file_url(url: str) -> bool:
+    """Check if a URL is a valid Google Cloud Storage file URL.
+
+    Such as `gs://bucket/path/to/file`.
+    """
+    return url.startswith("gs://")
