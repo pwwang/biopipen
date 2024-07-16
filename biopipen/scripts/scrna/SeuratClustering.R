b0d7e81e1b384d125670ee6a9fdaab1f8d61a3b6
pwwang <pwwang@pwwang.com>
2024-07-15 21:30:58 -0500

    refactor(scnra.SeuratClustering): refactor the script for better debugging

--- biopipen/scripts/scrna/SeuratClustering.R
+++ biopipen/scripts/scrna/SeuratClustering.R
@@ -23,50 +23,10 @@ options(str = strOptions(vec.len = 5, digits.d = 5))
 options(future.globals.maxSize = 80000 * 1024^2)
 plan(strategy = "multicore", workers = envs$ncores)
 
-.expand_dims <- function(args, name = "dims") {
-    # Expand dims from 30 to 1:30
-    if (is.numeric(args[[name]]) && length(args[[name]] == 1)) {
-        args[[name]] <- 1:args[[name]]
-    }
-    args
-}
+{{ biopipen_dir | joinpaths: "scripts", "scrna", "SeuratClustering-common.R" | source_r }}
 
-.expand_resolution <- function(resolution) {
-    expanded_res <- c()
-    for (res in resolution) {
-        if (is.numeric(res)) {
-            expanded_res <- c(expanded_res, res)
-        } else {
-            # is.character
-            parts <- trimws(unlist(strsplit(res, ",")))
-            for (part in parts) {
-                if (grepl(":", part)) {
-                    ps <- trimws(unlist(strsplit(part, ":")))
-                    if (length(ps) == 2) { ps <- c(ps, 0.1) }
-                    if (length(ps) != 3) {
-                        stop("Invalid resolution format: {part}. Expected 2 or 3 parts separated by ':' for a range.")
-                    }
-                    ps <- as.numeric(ps)
-                    expanded_res <- c(expanded_res, seq(ps[1], ps[2], by = ps[3]))
-                } else {
-                    expanded_res <- c(expanded_res, as.numeric(part))
-                }
-            }
-        }
-    }
-    # keep the last resolution at last
-    rev(unique(rev(round(expanded_res, 2))))
-}
-
-# recode clusters from 0, 1, 2, ... to c1, c2, c3, ...
-.recode_clusters <- function(clusters) {
-    recode <- function(x) paste0("c", as.integer(as.character(x)) + 1)
-    clusters <- factor(recode(clusters), levels = recode(levels(clusters)))
-    clusters
-}
-
-envs$RunUMAP <- .expand_dims(envs$RunUMAP)
-envs$FindNeighbors <- .expand_dims(envs$FindNeighbors)
+envs$RunUMAP <- expand_dims(envs$RunUMAP)
+envs$FindNeighbors <- expand_dims(envs$FindNeighbors)
 
 log_info("Reading Seurat object ...")
 sobj <- readRDS(srtfile)
@@ -86,115 +46,11 @@ if (is.character(envs$cache)) {
     writeLines(sobj_sig, file.path(cache_dir, "signature.txt"))
 }
 
-if (length(envs$ScaleData) > 0) {
-    if (DefaultAssay(sobj) == "SCT") {
-        stop("SCT assay detected, but ScaleData is specified. Use SCTransform instead.")
-    }
-    cached <- get_cached(envs$ScaleData, "ScaleData", cache_dir)
-    if (is.null(cached$data)) {
-        log_info("Running ScaleData ...")
-        envs$ScaleData$object <- sobj
-        sobj <- do_call(ScaleData, envs$ScaleData)
-        cached$data <- list(assay = sobj@assays$RNA, commands = sobj@commands)
-        save_to_cache(cached, "ScaleData", cache_dir)
-    } else {
-        log_info("Loading cached ScaleData ...")
-        sobj@assays$RNA <- cached$data$assay
-        sobj@commands <- cached$data$commands
-        DefaultAssay(sobj) <- "RNA"
-    }
-} else if (length(envs$SCTransform) > 0) {
-    if (DefaultAssay(sobj) != "SCT") {
-        stop("SCT assay not detected, but SCTransform is specified. Use ScaleData instead.")
-    }
-    cached <- get_cached(envs$SCTransform, "SCTransform", cache_dir)
-    asssay <- envs$SCTransform$new.assay.name %||% "SCT"
-    if (is.null(cached$data)) {
-        log_info("Running SCTransform ...")
-        envs$SCTransform$object <- sobj
-        sobj <- do_call(SCTransform, envs$SCTransform)
-        cached$data <- list(assay = sobj@assays$SCT, commands = sobj@commands)
-        save_to_cache(cached, "SCTransform", cache_dir)
-    } else {
-        log_info("Loading cached SCTransform ...")
-        sobj@assays[[assay]] <- cached$data$assay
-        sobj@commands <- cached$data$commands
-        DefaultAssay(sobj) <- assay
-    }
-}
-
-cached <- get_cached(envs$RunUMAP, "RunUMAP", cache_dir)
-reduc_name <- envs$RunUMAP$reduction.name %||% "umap"
-if (is.null(cached$data)) {
-    log_info("Running RunUMAP ...")
-    umap_args <- list_setdefault(
-        envs$RunUMAP,
-        object = sobj,
-        dims = 1:30,
-        reduction = sobj@misc$integrated_new_reduction %||% "pca"
-    )
-    ncells <- ncol(sobj)
-    umap_args$dims <- 1:min(max(umap_args$dims), ncells - 1)
-    umap_method <- envs$RunUMAP$umap.method %||% "uwot"
-    if (umap_method == "uwot" && is.null(envs$RunUMAP$n.neighbors)) {
-        # https://github.com/satijalab/seurat/issues/4312
-        umap_args$n.neighbors <- min(ncells - 1, 30)
-    }
-    sobj <- do_call(RunUMAP, umap_args)
-    cached$data <- list(reduc = sobj@reductions[[reduc_name]], commands = sobj@commands)
-    save_to_cache(cached, "RunUMAP", cache_dir)
-} else {
-    log_info("Loading cached RunUMAP ...")
-    sobj@reductions[[reduc_name]] <- cached$data$reduc
-    sobj@commands <- cached$data$commands
-}
-
-cached <- get_cached(envs$FindNeighbors, "FindNeighbors", cache_dir)
-if (is.null(cached$data)) {
-    log_info("Running FindNeighbors ...")
-    envs$FindNeighbors$object <- sobj
-    envs$FindNeighbors$reduction <- sobj@misc$integrated_new_reduction %||% "pca"
-    sobj <- do_call(FindNeighbors, envs$FindNeighbors)
-    cached$data <- list(graphs = sobj@graphs, commands = sobj@commands)
-    save_to_cache(cached, "FindNeighbors", cache_dir)
-} else {
-    log_info("Loading cached FindNeighbors ...")
-    sobj@graphs <- cached$data$graphs
-    sobj@commands <- cached$data$commands
-}
-
-envs$FindClusters$random.seed <- envs$FindClusters$random.seed %||% 8525
-resolution <- envs$FindClusters$resolution <- .expand_resolution(envs$FindClusters$resolution %||% 0.8)
-log_info("Running FindClusters at resolution: {paste(resolution, collapse=',')} ...")
-
-envs$FindClusters$object <- sobj
-envs$FindClusters$cluster.name <- paste0("seurat_clusters.", resolution)
-sobj <- do_call(FindClusters, envs$FindClusters)
-
-for (clname in envs$FindClusters$cluster.name) {
-    sobj@meta.data[[clname]] <- .recode_clusters(sobj@meta.data[[clname]])
-}
-sobj@meta.data$seurat_clusters <- .recode_clusters(sobj@meta.data$seurat_clusters)
-Idents(sobj) <- "seurat_clusters"
-
-ident_table <- table(Idents(sobj))
-log_info("- Found {length(ident_table)} clusters at resolution {resolution[length(resolution)]}")
-print(ident_table)
-cat("\n")
-
-if (DefaultAssay(sobj) == "SCT") {
-        # https://github.com/satijalab/seurat/issues/6968
-    log_info("Running PrepSCTFindMarkers ...")
-    sobj <- PrepSCTFindMarkers(sobj)
-    # compose a new SeuratCommand to record it to sobj@commands
-    scommand <- sobj@commands$FindClusters
-    scommand@name <- "PrepSCTFindMarkers"
-    scommand@time.stamp <- Sys.time()
-    scommand@assay.used <- "SCT"
-    scommand@call.string <- "PrepSCTFindMarkers(object = sobj)"
-    scommand@params <- list()
-    sobj@commands$PrepSCTFindMarkers <- scommand
-}
+sobj <- run_transformation(sobj)
+sobj <- run_umap(sobj)
+sobj <- run_findneighbors(sobj)
+sobj <- run_findclusters(sobj)
+sobj <- run_prepsctfindmarkers(sobj)
 
 log_info("Saving results ...")
 saveRDS(sobj, file = rdsfile)
