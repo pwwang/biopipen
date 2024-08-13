8bc341f2b33539add571c71e0b0f247dd38c4e17
pwwang <pwwang@pwwang.com>
2024-08-13 00:51:47 -0500

    fix(scrna.SeuratMap2Ref): fix report and add stats to report

--- biopipen/reports/scrna/SeuratMap2Ref.svelte
+++ biopipen/reports/scrna/SeuratMap2Ref.svelte
@@ -1,16 +1,20 @@
 {% from "utils/misc.liq" import report_jobs, table_of_images -%}
 <script>
-    import { Image } from "$libs";
+    import { Image, DataTable } from "$libs";
 </script>
 
 {%- macro report_job(job, h=1) -%}
-<h{{h}}>Reference UMAP</h{{h}}>
-{% set imgs = job.outdir | glob: "Reference_UMAP_*.png" %}
+
+<h{{h}}>UMAPs</h{{h}}>
+{% set imgs = job.outdir | glob: "UMAPs-*.png" %}
 {{ table_of_images(imgs) }}
 
-<h{{h}}>Query UMAP</h{{h}}>
-{% set imgs = job.outdir | glob: "Query_UMAP_*.png" %}
-{{ table_of_images(imgs) }}
+<h{{h}}>Stats</h{{h}}>
+{% for stfile in job.outdir | glob: "stats-*.txt" %}
+    <h{{h+1}}>{{stfile | stem | replace: "stats-", ""}}</h{{h+1}}>
+    <DataTable src="{{stfile}}" data={ {{stfile | datatable: sep="\t"}} } />
+{% endfor %}
+
 {%- endmacro -%}
 
 {%- macro head_job(job) -%}
