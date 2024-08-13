2f3e55068bfc8219af29fb835110693e3bae9c1a
pwwang <pwwang@pwwang.com>
2024-08-13 11:43:17 -0500

    refactor(scrna.Seurat2AnnData): abstract seurat_to_anndata() for reuse

--- biopipen/utils/single_cell.R
+++ biopipen/utils/single_cell.R
@@ -114,3 +114,63 @@ immdata_from_expanded <- function(
     )
     out
 }
+
+#' Convert Seurat object to Anndata
+#'
+#' @param sobjfile Seurat object file
+#' @param outfile Output file
+#' @param assay Assay to be used
+#'
+#' @export
+seurat_to_anndata <- function(sobjfile, outfile, assay = NULL, log_info, tmpdir = NULL, log_indent = "") {
+    library(Seurat)
+    library(SeuratDisk)
+    if (endsWith(sobjfile, ".rds") || endsWith(sobjfile, ".RDS")) {
+        library(digest)
+
+        dig <- digest::digest(sobjfile, algo = "md5")
+        dig <- substr(dig, 1, 8)
+        assay_name <- ifelse(is.null(assay), "", paste0("_", assay))
+        tmpdir <- tmpdir %||% dirname(outfile)
+        dir.create(tmpdir, showWarnings = FALSE)
+        h5seurat_file <- file.path(
+            tmpdir,
+            paste0(
+                tools::file_path_sans_ext(basename(outfile)),
+                assay_name, ".", dig, ".h5seurat"
+            )
+        )
+        if (file.exists(h5seurat_file) &&
+            (file.mtime(h5seurat_file) < file.mtime(sobjfile))) {
+            file.remove(h5seurat_file)
+        }
+        if (!file.exists(h5seurat_file)) {
+            log_info("{log_indent}Reading RDS file ...")
+            sobj <- readRDS(sobjfile)
+            assay <- assay %||% DefaultAssay(sobj)
+            # In order to convert to h5ad
+            # https://github.com/satijalab/seurat/issues/8220#issuecomment-1871874649
+            sobj$RNAv3 <- as(object = sobj[[assay]], Class = "Assay")
+            DefaultAssay(sobj) <- "RNAv3"
+            sobj$RNA <- NULL
+            sobj <- RenameAssays(sobj, RNAv3 = "RNA")
+
+            log_info("{log_indent}Saving to H5Seurat file ...")
+            SaveH5Seurat(sobj, h5seurat_file)
+            rm(sobj)
+            gc()
+            sobjfile <- h5seurat_file
+        } else {
+            log_info("{log_indent}Using existing H5Seurat file ...")
+        }
+    }
+
+    if (!endsWith(sobjfile, ".h5seurat")) {
+        stop(paste0("Unknown input file format: ",
+            tools::file_ext(sobjfile),
+            ". Supported formats: .rds, .RDS, .h5seurat"))
+    }
+
+    log_info("{log_indent}Converting to Anndata ...")
+    Convert(sobjfile, dest = outfile, assay = assay %||% "RNA", overwrite = TRUE)
+}
