d86b529ce30248cf54795e195f4175c26d61219e
pwwang <pwwang@pwwang.com>
2026-03-03 09:58:01 -0700

    fix(regulatory): correct function calls to use biopipen.utils for height and width calculations in plot_variant_motifs

--- biopipen/scripts/regulatory/motifs-common.R
+++ biopipen/scripts/regulatory/motifs-common.R
@@ -307,13 +307,13 @@ plot_variant_motifs <- function(results, variant, devpars, outdir) {
         stop(sprintf("Variant %s not found in results", variant))
     }
     devpars$res <- devpars$res %||% 100
-    devpars$height <- get_worh(2.4 * devpars$res + length(res) * 1.2 * devpars$res, devpars$height)
+    devpars$height <- biopipen.utils:::get_worh(2.4 * devpars$res + length(res) * 1.2 * devpars$res, devpars$height)
     if (is.null(devpars$width)) {
         left <- min(sapply(res$motifPos, `[`, 1))
         right <- max(sapply(res$motifPos, `[`, 2))
         width <- 1.5 * devpars$res + (right - left) * 0.3 * devpars$res
         width <- max(devpars$width, 5 * devpars$res)
-        devpars$width <- get_worh(width, devpars$width)
+        devpars$width <- biopipen.utils:::get_worh(width, devpars$width)
     }
 
     plotfile <- file.path(plotdir, sprintf("%s.png", slugify(variant)))
